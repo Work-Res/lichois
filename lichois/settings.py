8020087edbd40e98f2530c8618f4d44a4424316e
Coulson Kgathi <ckgathi@gmail.com>
2024-04-10 13:27:28 +0200

    rest api settings

--- lichois/settings.py
+++ lichois/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     
+    'rest_framework',
+    
 ]
 
 MIDDLEWARE = [
@@ -111,6 +113,11 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 
 # Static files (CSS, JavaScript, Images)
