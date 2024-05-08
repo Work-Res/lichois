2d6ebe82dca18e5b0f15990f64c05db37f9750f8
moffatmore <mofenyimoffat@gmail.com>
2024-05-08 14:48:45 +0200

    🐛 fix(Board): Fix board

--- board/admin/board_admin.py
+++ board/admin/board_admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from ..admin_site import decision_admin
+from ..admin_site import board_admin
 from ..models import Board
 from ..forms import BoardForm
 
 
-@admin.register(Board, site=decision_admin)
+@admin.register(Board, site=board_admin)
 class BoardAdmin(admin.ModelAdmin):
 
     form = BoardForm
--- board/admin_site.py
+++ board/admin_site.py
@@ -2,11 +2,11 @@ from django.contrib.admin import AdminSite as DjangoAdminSite
 
 
 class AdminSite(DjangoAdminSite):
+	site_title = 'Board'
+	site_header = 'Board'
+	index_title = 'Board'
+	site_url = '/administration/'
+	enable_nav_sidebar = False
 
-    site_title = 'Board'
-    site_header = 'Board'
-    index_title = 'Board'
-    site_url = '/administration/'
-    enable_nav_sidebar = False
 
-decision_admin = AdminSite(name='decision_admin')
+board_admin = AdminSite(name='board')
