75f8641ee735f3088089f3e1405fc7c4892a084d
Itumeleng <imosweu@burs.org.bw>
2024-05-10 18:18:28 +0200

    pep8

--- citizenship/models/naturalisation.py
+++ citizenship/models/naturalisation.py
@@ -10,21 +10,13 @@ class Naturalisation(BaseUuidModel):
     #ContactInfo
 
     birth_citizenship = models.CharField(max_length=190)
-
     present_citizenship = models.CharField(max_length=190)
-
     investment_level = models.CharField(max_length=190)
-
     prev_application_date = models.DateField()
-
     name_change_particulars = models.TextField(max_length=190)
-
     citizenship_change_particulars = models.TextField(max_length=190)
-
     lost_citizenship_circumstances = models.TextField(max_length=190)
-
     previous_convictions = models.TextField(max_length=190)
-
     botswana_relations = models.TextField(max_length=190)
 
     class Meta:
--- citizenship/models/oath_of_allegiance.py
+++ citizenship/models/oath_of_allegiance.py
@@ -3,6 +3,6 @@ from base_module.model_mixins import BaseUuidModel
 
 
 class OathOfAllegiance(BaseUuidModel):
-
+    #TODO: oath of allegiance
     class Meta:
         app_label = 'citizenship'
