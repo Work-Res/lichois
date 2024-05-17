20ade6f694b75a37fb2d0083a63520d1067d43d8
moffatmore <mofenyimoffat@gmail.com>
2024-05-17 14:26:21 +0200

    🔥 fix(BoardMeeting): fix serializer, include board id from board member who is currently logged in

--- board/viewsets/board_meeting_vewset.py
+++ board/viewsets/board_meeting_vewset.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.response import Response
 
 from ..models import BoardMeeting
 from ..serializers import BoardMeetingSerializer
@@ -7,5 +8,11 @@ from ..serializers import BoardMeetingSerializer
 class BoardMeetingViewSet(viewsets.ModelViewSet):
 	queryset = BoardMeeting.objects.all()
 	serializer_class = BoardMeetingSerializer
+	
+	def create(self, request, *args, **kwargs):
+		serializer = self.get_serializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		serializer.save(request=request)
+		return Response(serializer.data, status=status.HTTP_201_CREATED)
 	
 	
