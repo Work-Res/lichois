df51e31401498855c0b4f83b6c8d145dba618490
moffatmore <mofenyimoffat@gmail.com>
2024-05-17 14:34:19 +0200

    🔥 fix(BoardMeeting): fix serializer, include board id from board member who is currently logged in

--- board/viewsets/board_meeting_vewset.py
+++ board/viewsets/board_meeting_vewset.py
@@ -1,18 +1,30 @@
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 
-from ..models import BoardMeeting
+from app.api.common.web import APIMessage
+from ..models import BoardMeeting, BoardMember
 from ..serializers import BoardMeetingSerializer
 
 
 class BoardMeetingViewSet(viewsets.ModelViewSet):
 	queryset = BoardMeeting.objects.all()
 	serializer_class = BoardMeetingSerializer
-	
-	def create(self, request, *args, **kwargs):
-		serializer = self.get_serializer(data=request.data)
-		serializer.is_valid(raise_exception=True)
-		serializer.save(request=request)
-		return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+def create(self, request, *args, **kwargs):
+	board = BoardMember.objects.filter(user=request.user).first().board
+	if not board:
+		api_message = APIMessage(
+			code=400,
+			message="Bad request",
+			details="User is not a member of any board"
+		)
+		return Response(data=api_message.to_dict(), status=status.HTTP_400_BAD_REQUEST)
+	mutable_data = request.data.copy()  # Make a mutable copy
+	mutable_data['board'] = board.id  # Modify the data
+	serializer = self.get_serializer(data=mutable_data)
+	serializer.is_valid(raise_exception=True)
+	serializer.save()
+	return Response(serializer.data, status=status.HTTP_201_CREATED)
 	
 	
