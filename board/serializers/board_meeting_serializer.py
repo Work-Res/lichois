915d130c0627758b31f1a3d980e93cf38520823a
moffatmore <mofenyimoffat@gmail.com>
2024-05-17 14:16:26 +0200

    🔥 fix(BoardMeeting): fix serializer, include board id from board member who is currently logged in

--- board/serializers/board_meeting_serializer.py
+++ board/serializers/board_meeting_serializer.py
@@ -1,18 +1,35 @@
-from rest_framework import serializers
-from ..models import BoardMeeting
+from rest_framework import serializers, status
+from rest_framework.response import Response
+
+from app.api.common.web import APIMessage
+from ..models import BoardMeeting, BoardMember
 
 
 class BoardMeetingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BoardMeeting
-        fields = (
-            'id',
-            'title',
-            'meeting_date',
-            'description',
-            'status',
-            'minutes',
-            'meeting_type',
-            'location',
-            'board',
-        )
+	class Meta:
+		model = BoardMeeting
+		fields = (
+			'id',
+			'title',
+			'meeting_date',
+			'description',
+			'status',
+			'minutes',
+			'meeting_type',
+			'location',
+			'board',
+		)
+	
+	def create(self, validated_data):
+		request = self.context.get('request')
+		auth_user = request.user
+		board = BoardMember.objects.filter(user=auth_user).first().board
+		if not board:
+			api_message = APIMessage(
+				code=400,
+				message="Bad request",
+				details="User is not a member of any board"
+			)
+			return Response(data=api_message.to_dict(), status=status.HTTP_400_BAD_REQUEST)
+		validated_data['board'] = board
+		return super().create(validated_data)
--- board/viewsets/board_meeting_vewset.py
+++ board/viewsets/board_meeting_vewset.py
@@ -1,28 +1,11 @@
-from rest_framework import status, viewsets
-from rest_framework.response import Response
+from rest_framework import viewsets
 
-from app.api.common.web import APIMessage
-from ..models import BoardMeeting, BoardMember
+from ..models import BoardMeeting
 from ..serializers import BoardMeetingSerializer
 
 
 class BoardMeetingViewSet(viewsets.ModelViewSet):
 	queryset = BoardMeeting.objects.all()
 	serializer_class = BoardMeetingSerializer
-	
-	def create(self, request, *args, **kwargs):
-		auth_user = request.user
-		board = BoardMember.objects.filter(user=auth_user).first().board
-		if not board:
-			api_message = APIMessage(
-				code=400,
-				message="Bad request",
-				details="User is not a member of any board"
-			)
-			return Response(data=api_message.to_dict(), status=status.HTTP_400_BAD_REQUEST)
-		data = request.data.copy()  # Make a mutable copy
-		data['board'] = board.id  # Modify the copy
-		request._data = data  # Assign the modified copy back to request.data
-		return super().create(request, *args, **kwargs)
 	
 	
