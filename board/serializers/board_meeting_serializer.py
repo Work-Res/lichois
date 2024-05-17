ea1685154e75c6d99f77c68523d994b005e74f00
moffatmore <mofenyimoffat@gmail.com>
2024-05-17 14:35:48 +0200

    🔥 fix(BoardMeeting): fix serializer, include board id from board member who is currently logged in

--- board/serializers/board_meeting_serializer.py
+++ board/serializers/board_meeting_serializer.py
@@ -20,16 +20,16 @@ class BoardMeetingSerializer(serializers.ModelSerializer):
 			'board',
 		)
 	
-	# def create(self, validated_data):
-	# 	request = self.context.get('request')
-	# 	auth_user = request.user
-	# 	board = BoardMember.objects.filter(user=auth_user).first().board
-	# 	if not board:
-	# 		api_message = APIMessage(
-	# 			code=400,
-	# 			message="Bad request",
-	# 			details="User is not a member of any board"
-	# 		)
-	# 		return Response(data=api_message.to_dict(), status=status.HTTP_400_BAD_REQUEST)
-	# 	validated_data['board'] = board
-	# 	return super().create(validated_data)
+	def create(self, validated_data):
+		request = self.context.get('request')
+		auth_user = request.user
+		board = BoardMember.objects.filter(user=auth_user).first().board
+		if not board:
+			api_message = APIMessage(
+				code=400,
+				message="Bad request",
+				details="User is not a member of any board"
+			)
+			return Response(data=api_message.to_dict(), status=status.HTTP_400_BAD_REQUEST)
+		validated_data['board'] = board
+		return super().create(validated_data)
