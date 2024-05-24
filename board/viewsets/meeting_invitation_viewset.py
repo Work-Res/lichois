149ae4c206fea4d73c842b2051cb0a982d5d61a8
moffatmore <mofenyimoffat@gmail.com>
2024-05-25 00:10:17 +0200

    🔥 fix(MeetingInvitation): get meeting invitations for a current logged-in board member

--- board/viewsets/meeting_invitation_viewset.py
+++ board/viewsets/meeting_invitation_viewset.py
@@ -1,18 +1,30 @@
 from rest_framework import viewsets
-from ..models import MeetingInvitation
+from rest_framework.exceptions import PermissionDenied
+
+from app.api.common.web import APIMessage
+from ..models import BoardMember, MeetingInvitation
 from ..serializers import MeetingInvitationSerializer
 
 
 class MeetingInvitationViewSet(viewsets.ModelViewSet):
 	serializer_class = MeetingInvitationSerializer
+	queryset = MeetingInvitation.objects.all()
 	
 	def get_queryset(self):
 		"""
         Optionally restricts the returned invitations to a given user,
         by filtering against a `invited_user` query parameter in the URL.
         """
-		queryset = MeetingInvitation.objects.all()
-		invited_user = self.request.query_params.get('invited_user', None)
-		if invited_user is not None:
-			queryset = queryset.filter(invited_user__id=invited_user)
-		return queryset
+		invited_user = self.request.user
+		try:
+			board_member = BoardMember.objects.get(user=invited_user)
+		except BoardMember.DoesNotExist:
+			api_message = APIMessage(
+				code=400,
+				message="Bad request",
+				details="User is not a member of any board"
+			)
+			raise PermissionDenied(api_message.to_dict())
+		else:
+			self.queryset = MeetingInvitation.filter(invited_user=board_member)
+		return self.queryset
