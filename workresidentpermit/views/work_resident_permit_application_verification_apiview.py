7849a6c8d397536672b140ff46f3cdc4a6762cff
Tshepiso Setsiba <tsetsiba@burs.org.bw>
2024-05-25 00:44:48 +0200

    fix verification api

--- workresidentpermit/views/work_resident_permit_application_verification_apiview.py
+++ workresidentpermit/views/work_resident_permit_application_verification_apiview.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from app.api.serializers import ApplicationVerificationRequestSerializer
+from app.api import ApplicationVerificationRequest
 
 from ..validators import WorkResidentPermitVerificationValidator
 from ..classes import WorkResidentPermitApplication
@@ -16,8 +17,9 @@ class WorkPermitApplicationVerificationAPIView(APIView):
             validator = WorkResidentPermitVerificationValidator(document_number=document_number)
             if validator.is_valid():
                 print("verification_request=serializer.data: ", serializer.data)
+                verification_request = ApplicationVerificationRequest(**serializer.data)
                 application = WorkResidentPermitApplication(
-                    document_number=document_number, verification_request=serializer.data)
+                    document_number=document_number, verification_request=verification_request)
                 data = application.submit_verification().result()
                 return Response(data, status=status.HTTP_200_OK)
             else:
