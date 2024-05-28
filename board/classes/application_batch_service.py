aa5f8939f0fbb315636327498f5baa1b2e576abe
Tshepiso Setsiba <tsetsiba@burs.org.bw>
2024-05-28 14:19:35 +0200

    fix many=True saving django model

--- board/classes/application_batch_service.py
+++ board/classes/application_batch_service.py
@@ -10,7 +10,9 @@ class ApplicationBatchService:
         self.application_batch_request = application_batch_request
 
     def create_batch(self):
-        ApplicationBatch.objects.create(
-            applications=Application.objects.filter(id__in=self.application_batch_request.applications),
+        batch = ApplicationBatch.objects.create(
             batch_type=self.application_batch_request.batch_type
         )
+        for application in Application.objects.filter(id__in=self.application_batch_request.applications):
+            batch.add(application)
+        batch.save()
