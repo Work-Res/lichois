837b59c2193eac41049faea09c0e3c1de0ab8e3f
Tshepiso Setsiba <tsetsiba@burs.org.bw>
2024-05-29 11:42:18 +0200

    fix api

--- workresidentpermit/api/serializers/security_clearance_serializer.py
+++ workresidentpermit/api/serializers/security_clearance_serializer.py
@@ -1,17 +1,12 @@
 from rest_framework import serializers
 from ...models import SecurityClearance
 
-from .residence_permit_serializer import ResidencePermitSerializer
-
 
 class SecurityClearanceSerializer(serializers.ModelSerializer):
-
-    # work_resident_permit = WorkResidencePermitSerializer()
 
     class Meta:
         model = SecurityClearance
         fields = (
-            'work_resident_permit',
             'date_requested',
             'date_approved',
             'status',
