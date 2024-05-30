99817571de28122d2b524bc8b28e0e715773913d
moffatmore <mofenyimoffat@gmail.com>
2024-05-30 16:15:44 +0200

    :fire: fix(AgendaItem): Fixed agenda items get by agenda using query

--- board/viewsets/agenda_item_viewset.py
+++ board/viewsets/agenda_item_viewset.py
@@ -8,10 +8,21 @@ from ..serializers import AgendaItemSerializer
 class AgendaItemViewSet(viewsets.ModelViewSet):
 	queryset = AgendaItem.objects.all()
 	serializer_class = AgendaItemSerializer
-	lookup_field = 'agenda'
 	
 	def retrieve(self, request, agenda,  *args, **kwargs):
 		queryset = AgendaItem.objects.filter(agenda__id=agenda)
 		serializer = AgendaItemSerializer(queryset, many=True)
 		return Response(serializer.data)
+	
+	def get_queryset(self):
+		"""
+		Optionally restricts the returned agenda items to a given agenda,
+		by filtering against a `agenda` query parameter in the URL.
+		"""
+		agenda = self.request.query_params.get('agenda', None)
+		if agenda is not None:
+			queryset = AgendaItem.objects.filter(agenda__id=agenda)
+		else:
+			queryset = AgendaItem.objects.all()
+		return queryset
 
