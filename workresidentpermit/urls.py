557f0fba83150c1baec1d83a5bf805bcf8bc8f15
moffatmore <mofenyimoffat@gmail.com>
2024-06-08 23:29:20 +0200

    🔥 fix(WorkResidentApp): Refactor urls for workresident

--- workresidentpermit/urls.py
+++ workresidentpermit/urls.py
@@ -21,16 +21,25 @@ router.register(r'permit-cancellation', PermitCancellationViewSet, basename='per
 
 
 urlpatterns = [
-    path('workresidentpermit/summary/<str:document_number>', WorkResidentPermitApplicationDetailView.as_view(),
-         name='work_resident_permit_detail'),
+   
     path('spouse/<str:document_number>/<str:pk>', SpouseCreateListView.as_view({'get': 'list'}),
          name='spouse-detail'),
+    
+    #  New generic endpoints
+    path('verification/<str:document_number>/submit/',
+         WorkPermitApplicationVerificationAPIView.as_view(), name='submit-verification'),
+    path('security_clearance/<str:document_number>/submit/',
+         SecurityClearanceCreateAPIView.as_view(), name='submit-security-clearance'),
+    
+    # Old endpoints
+    path('workpermit/<str:document_number>/submit/verification',
+         WorkPermitApplicationVerificationAPIView.as_view(), name='submit-work-res-verification'),
+    path('workpermit/<str:document_number>/submit/security_clearance',
+         SecurityClearanceCreateAPIView.as_view(), name='submit-work-res-security-clearance'),
     path('workpermit/<str:document_number>/submit', WorkPermitApplicationAPIView.as_view(),
          name='submit-workresident-permit'),
-    path('workpermit/<str:document_number>/submit/verification',
-         WorkPermitApplicationVerificationAPIView.as_view(), name='submit-verification'),
-    path('workpermit/<str:document_number>/submit/security_clearance',
-         SecurityClearanceCreateAPIView.as_view(), name='submit-security-clearance'),
+    path('workresidentpermit/summary/<str:document_number>', WorkResidentPermitApplicationDetailView.as_view(),
+         name='work_resident_permit_detail'),
     path('', include(router.urls)),
 ]
 
