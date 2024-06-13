576bccd4239a6ed1e39a4d8b1132db8d14480613
moffatmore <mofenyimoffat@gmail.com>
2024-06-13 17:31:41 +0200

    ✨  fix(InterestDeclaration):get interest declaration of current logged in meeting attendee

--- board/viewsets/interest_declaration_viewset.py
+++ board/viewsets/interest_declaration_viewset.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from app.api.common.web import APIMessage
-from ..models import BoardMember, InterestDeclaration
+from ..choices import PRESENT
+from ..models import BoardMember, InterestDeclaration, MeetingAttendee
 from ..serializers import InterestDeclarationSerializer
 
 
@@ -10,13 +12,17 @@ class InterestDeclarationViewSet(viewsets.ModelViewSet):
 	queryset = InterestDeclaration.objects.all()
 	serializer_class = InterestDeclarationSerializer
 	
-	# def create(self, request, *args, **kwargs):
-	# 	# if interest declaration exists, raise an error
-	# 	document_number = request.data.get('document_number')
-	# 	board_member = BoardMember.objects.filter(user=request.user).first()
-	# 	if not board_member:
-	# 		return Response(APIMessage(message='User is not a member of any board', code=400).to_dict())
-	# 	meting_attendee = MeetingAttendee.objects.filter(Q(board_member=board_member) & Q(attendance_status=PRESENT) & Q(meeting=request.data.get('meeting'))).first()
-	# 	if InterestDeclaration.objects.filter(document_number=document_number).exists():
-	# 		return Response(APIMessage(message='Interest declaration already exists', code=500).to_dict())
-	# 	return super().create(request, *args, **kwargs)
+	@action(detail=False, methods=['get'], url_path='current-attendee/(?P<document_number>[A-Za-z0-9-]+)',)
+	def check_interest_declaration(self, request, document_number):
+		board_member = BoardMember.objects.filter(user=request.user).first()
+		if not board_member:
+			return Response(APIMessage(message='User is not a member of any board', code=400).to_dict())
+		meeting_attendee = MeetingAttendee.objects.filter(board_member=board_member, attendance_status=PRESENT).first()
+		if not meeting_attendee:
+			return Response(APIMessage(message='User is not an attendee of board meeting', code=400).to_dict())
+		interest_declaration = InterestDeclaration.objects.filter(meeting_attendee=meeting_attendee,
+		                                                          document_number=document_number).first()
+		if interest_declaration:
+			return Response(data=InterestDeclarationSerializer(interest_declaration).data)
+		return Response(APIMessage(message='Interest declaration not found', code=404).to_dict())
+	
