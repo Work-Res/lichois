47348c6a7e22cb8b86769c352c9d7d06b9ecb24f
Tshepiso Setsiba <tsetsiba@burs.org.bw>
2024-06-15 01:24:03 +0200

    production

--- workresidentpermit/views/production_permit_view.py
+++ workresidentpermit/views/production_permit_view.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 
 from workresidentpermit.validators import ProductionValidator
 
-from workresidentpermit.classes.production import FetchRequiredDataForProduction
+from workresidentpermit.classes.production import PermitDataProcessor
 
 
 class ProductionPermitView(APIView):
@@ -18,8 +18,8 @@ class ProductionPermitView(APIView):
             file_name = "work_resident_permit_only.json"
             configuration_file = os.path.join(
                 os.getcwd(), "workresidentpermit", "classes", "production", "configuration", file_name)
-            data_fetcher = FetchRequiredDataForProduction(
+            permit_data = PermitDataProcessor(
                 configuration_file_name=configuration_file, document_number=document_number)
-            return Response(data_fetcher.get_data(), status=status.HTTP_200_OK)
+            return Response(permit_data.transform_data(), status=status.HTTP_200_OK)
         else:
             return Response(validator.response.messages, status=status.HTTP_400_BAD_REQUEST)
