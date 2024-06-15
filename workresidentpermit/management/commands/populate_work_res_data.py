c3815236276a61202d5c6297f17b40117d9b0ec3
moffatmore <mofenyimoffat@gmail.com>
2024-06-15 04:13:13 +0200

    🔥  fix(ApplicationStatusEnum): fix application status enums

--- workresidentpermit/management/commands/populate_work_res_data.py
+++ workresidentpermit/management/commands/populate_work_res_data.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from django.db.transaction import atomic
 from app.api import NewApplicationDTO
 from app.classes import ApplicationService
-from app.utils.system_enums import ApplicationStatusEnum
+from app.utils.system_enums import ApplicationProcesses, ApplicationStatusEnum
 from app_personal_details.models import Passport, Person
 from app_address.models import ApplicationAddress, Country
 from app_contact.models import ApplicationContact
