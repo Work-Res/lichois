5657e5060647db5302c98adcc93d46583740d9cd
Tumisang Tshimologo <156487673+TTshimo@users.noreply.github.com>
2024-06-16 16:56:24 +0200

    Update apps.py
    Signed-off-by: Tumisang Tshimologo <156487673+TTshimo@users.noreply.github.com>

--- travel/apps.py
+++ travel/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-class WorkresidentpermitConfig(AppConfig):
+class TravelConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'travel'
     verbose_name = "Travel Module"
