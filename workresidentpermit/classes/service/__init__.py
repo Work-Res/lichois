a9cbc1b17a45c6adee1ffe4a5478d8a0c77a1fed
Tshepiso Setsiba <tsetsiba@burs.org.bw>
2024-06-16 20:47:33 +0200

    add cancellation init

--- workresidentpermit/classes/service/__init__.py
+++ workresidentpermit/classes/service/__init__.py
@@ -3,3 +3,4 @@ from .recommendation_service import RecommendationService
 from .security_clearance_service import SecurityClearanceService
 from .work_resident_permit_decision_service import WorkResidentPermitDecisionService
 from .special_permit_decision_service import SpecialPermitDecisionService
+from .permit_cancellation_decision_service import PermitCancellationDecisionService
