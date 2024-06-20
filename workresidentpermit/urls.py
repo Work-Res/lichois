21a376fdc094a87e64dddadff47abcd10ad6f914
Tshepiso Setsiba <tsetsiba@burs.org.bw>
2024-06-20 17:38:51 +0200

    fix PEP8 format

--- workresidentpermit/urls.py
+++ workresidentpermit/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path, re_path
 from rest_framework.routers import DefaultRouter
 
 from .views import (ChildCreateListView, CommissionerDecisionAPIView, EmergencyResidencePermitViewSet,
-                    ExemptionCertificateViewSet, PermitCancellationViewSet,PermitAppealViewSet,
+                    ExemptionCertificateViewSet, PermitCancellationViewSet, PermitAppealViewSet,
                     ResidencePermitViewSet, SpouseCreateListView, WorkPermitViewSet,
                     WorkResidentPermitApplicationDetailView, WorkPermitApplicationAPIView,
                     WorkPermitApplicationVerificationAPIView, MinisterDecisionAPIView,
@@ -19,32 +19,31 @@ router.register(r'exemption-certificate', ExemptionCertificateViewSet, basename=
 router.register(r'permit-cancellation', PermitCancellationViewSet, basename='permit-cancellation')
 router.register(r'permit-appeal', PermitAppealViewSet, basename='permit-appeal')
 urlpatterns = [
-	
-	path('spouse/<str:document_number>/<str:pk>', SpouseCreateListView.as_view({'get': 'list'}),
-	     name='spouse-detail'),
-	
-	#  New generic endpoints
-	path('verification/<str:document_number>/submit/',
-	     WorkPermitApplicationVerificationAPIView.as_view(), name='submit-verification'),
-	path('security_clearance/<str:document_number>/submit/',
-	     SecurityClearanceCreateAPIView.as_view(), name='submit-security-clearance'),
-	path('commissioner-decision/', CommissionerDecisionAPIView.as_view(), name='commissioner-decision-create'),
-	path('minister-decision/', MinisterDecisionAPIView.as_view(), name='minister-decision-create'),
-	
-	# Old endpoints
-	path('workpermit/<str:document_number>/submit/verification',
-	     WorkPermitApplicationVerificationAPIView.as_view(), name='submit-work-res-verification'),
-	path('workpermit/<str:document_number>/submit/security_clearance',
-	     SecurityClearanceCreateAPIView.as_view(), name='submit-work-res-security-clearance'),
-	path('workpermit/<str:document_number>/submit', WorkPermitApplicationAPIView.as_view(),
-	     name='submit-workresident-permit'),
-	path('workresidentpermit/summary/<str:document_number>', WorkResidentPermitApplicationDetailView.as_view(),
-	     name='work_resident_permit_detail'),
-	
-	path('production/<str:document_number>', ProductionPermitView.as_view(),
-	     name='production-permit'),
-	path('production/travel_certificate/<str:document_number>', TravelCertificateView.as_view(),
-		 name='production-permit'),
-	path('', include(router.urls)),
-]
 
+    path('spouse/<str:document_number>/<str:pk>', SpouseCreateListView.as_view({'get': 'list'}),
+         name='spouse-detail'),
+
+    #  New generic endpoints
+    path('verification/<str:document_number>/submit/',
+         WorkPermitApplicationVerificationAPIView.as_view(), name='submit-verification'),
+    path('security_clearance/<str:document_number>/submit/',
+         SecurityClearanceCreateAPIView.as_view(), name='submit-security-clearance'),
+    path('commissioner-decision/', CommissionerDecisionAPIView.as_view(), name='commissioner-decision-create'),
+    path('minister-decision/', MinisterDecisionAPIView.as_view(), name='minister-decision-create'),
+
+    # Old endpoints
+    path('workpermit/<str:document_number>/submit/verification',
+         WorkPermitApplicationVerificationAPIView.as_view(), name='submit-work-res-verification'),
+    path('workpermit/<str:document_number>/submit/security_clearance',
+         SecurityClearanceCreateAPIView.as_view(), name='submit-work-res-security-clearance'),
+    path('workpermit/<str:document_number>/submit', WorkPermitApplicationAPIView.as_view(),
+         name='submit-workresident-permit'),
+    path('workresidentpermit/summary/<str:document_number>', WorkResidentPermitApplicationDetailView.as_view(),
+         name='work_resident_permit_detail'),
+
+    path('production/<str:document_number>', ProductionPermitView.as_view(),
+         name='production-permit'),
+    path('production/travel_certificate/<str:document_number>', TravelCertificateView.as_view(),
+         name='production-permit'),
+    path('', include(router.urls)),
+]
