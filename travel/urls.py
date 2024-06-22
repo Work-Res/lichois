ae1b788e491748795494650f481bd24137f64ffe
Tumisang Tshimologo <156487673+TTshimo@users.noreply.github.com>
2024-06-22 22:51:44 +0200

    Add files via upload
    Signed-off-by: Tumisang Tshimologo <156487673+TTshimo@users.noreply.github.com>

--- travel/urls.py
+++ travel/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import TravelCertificateViewSet
+from .views import TravelCertificatePermitViewSet
 
 router = DefaultRouter()
 router.register(r'travel-certificates', TravelCertificateViewSet)
+router.register(r'travel-certificate-permits', TravelCertificatePermitViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
