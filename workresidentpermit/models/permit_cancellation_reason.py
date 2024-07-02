fd15ba1e18843f8c05935ead0988d1736557f984
moffatmore <mofenyimoffat@gmail.com>
2024-07-02 22:55:46 +0200

    ✨  fix(PermitCancellationReason): Fix permit cancellation admin

--- workresidentpermit/models/permit_cancellation_reason.py
+++ workresidentpermit/models/permit_cancellation_reason.py
@@ -1,15 +1,14 @@
+from base_module.model_mixins import BaseUuidModel
 from django.db import models
 
-from app.models import ApplicationBaseModel
 
-
-class PermitCancellationReason(ApplicationBaseModel):
-    reason_for_cancellation = models.CharField(max_length=255)
-    other_reason = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.reason_for_cancellation
-    
-    class Meta:
-        verbose_name_plural = "Permit Cancellation Reason"
-        ordering = ['-created']
+class PermitCancellationReason(BaseUuidModel):
+	reason_for_cancellation = models.CharField(max_length=255)
+	other_reason = models.TextField(blank=True, null=True)
+	
+	def __str__(self):
+		return self.reason_for_cancellation
+	
+	class Meta:
+		verbose_name_plural = "Permit Cancellation Reason"
+		ordering = ['-created']
