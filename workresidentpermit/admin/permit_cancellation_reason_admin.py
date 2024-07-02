1f29442df47cf1a27799ab129c8e52668c1dcefc
moffatmore <mofenyimoffat@gmail.com>
2024-07-02 22:49:36 +0200

    ✨  feat(PermitCancellationReason): Added permit cancellation admin

--- workresidentpermit/admin/permit_cancellation_reason_admin.py
+++ workresidentpermit/admin/permit_cancellation_reason_admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from ..models import PermitCancellationReason
 
 
-@admin.register(PermitCancellationReason)
 class PermitCancellationReasonAdmin(admin.ModelAdmin):
-    list_display = ('reason_for_cancellation', 'created_at', 'updated_at')
-    search_fields = ('reason_for_cancellation',)
+	list_display = ('reason_for_cancellation', 'created_at', 'updated_at')
+	search_fields = ('reason_for_cancellation',)
+
+
+admin.site.register(PermitCancellationReason, PermitCancellationReasonAdmin)
