5def007e90b57f9c5290b31329fa18c0f83ade80
moffatmore <mofenyimoffat@gmail.com>
2024-07-02 09:46:26 +0200

    🔥 fix(InvestorAssessmentFilter): fix investor assessment model filter document number

--- app_assessment/views/assessment_investor_create_view.py
+++ app_assessment/views/assessment_investor_create_view.py
@@ -8,25 +8,24 @@ from ..models import Assessment, AssessmentInvestor
 
 
 class AssessmentInvestorModelFilter(django_filters.FilterSet):
-
-    document_number = django_filters.CharFilter(
-        field_name='document_number', lookup_expr='exact')
-
-    min_result_date = django_filters.DateFilter(field_name='created', lookup_expr='gte')
-    max_result_date = django_filters.DateFilter(field_name='created', lookup_expr='lte')
-
-    class Meta:
-        model = AssessmentInvestor
-        exclude = ('user_created', 'marking_score')
+	document_number = django_filters.CharFilter(
+		field_name='document_number', lookup_expr='exact')
+	
+	min_result_date = django_filters.DateFilter(field_name='created', lookup_expr='gte')
+	max_result_date = django_filters.DateFilter(field_name='created', lookup_expr='lte')
+	
+	class Meta:
+		model = AssessmentInvestor
+		exclude = ('user_created', 'marking_score')
 
 
 class AssessmentInvestorViewSet(viewsets.ModelViewSet):
-    lookup_field = 'document_number'
-    queryset = AssessmentInvestor.objects.all()
-    serializer_class = AssessmentInvestorSerializer
-    filterset_class = AssessmentInvestorModelFilter
-
-    def retrieve(self, request, document_number, *args, **kwargs):
-        assessment = AssessmentInvestor.objects.filter(document_number=document_number)
-        serializer = AssessmentInvestorSerializer(assessment, many=True)
-        return Response(serializer.data)
+	lookup_field = 'document_number'
+	queryset = AssessmentInvestor.objects.all()
+	serializer_class = AssessmentInvestorSerializer
+	filterset_class = AssessmentInvestorModelFilter
+	
+	def retrieve(self, request, document_number=None, *args, **kwargs):
+		assessment = AssessmentInvestor.objects.filter(document_number=document_number)
+		serializer = AssessmentInvestorSerializer(assessment, many=True)
+		return Response(serializer.data)
