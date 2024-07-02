bae631ea0e69f5349775b99b8058d8579bbe4255
moffatmore <mofenyimoffat@gmail.com>
2024-07-02 11:26:29 +0200

    🔥 fix(InvestorAssessmentSerializer): fix investor assessment investor serializers

--- app_assessment/api/serializers.py
+++ app_assessment/api/serializers.py
@@ -47,7 +47,8 @@ class AssessmentInvestorSerializer(serializers.ModelSerializer):
         file_location = os.path.join(os.getcwd(), "app_assessment", "data", "assessments", file_name)
         reader = ReadJSON(file_location=file_location)
 
-        assessment = Assessment(**data)
+        # assessment = Assessment(**data)
+        assessment = Assessment.objects.create(**data)
         validator = AssessmentValidator(assessment=assessment, rules=reader.json_data())
         if not validator.is_valid():
             raise serializers.ValidationError(validator.response.result())
