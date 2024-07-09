db575ae3ae0292ffd1788a2d720c74b7a5346653
moffatmore <mofenyimoffat@gmail.com>
2024-07-09 14:39:48 +0200

    Fix board meeting interest declaration, return full name of metting attendee

--- board/models/meeting_attendee.py
+++ board/models/meeting_attendee.py
@@ -7,27 +7,20 @@ from ..choices import ATTENDANCE_STATUS
 
 class MeetingAttendee(BaseUuidModel):
 
-    meeting = models.ForeignKey(
-        BoardMeeting,
-        on_delete=models.CASCADE
-    )
+    meeting = models.ForeignKey(BoardMeeting, on_delete=models.CASCADE)
 
-    board_member = models.ForeignKey(
-        BoardMember,
-        on_delete=models.CASCADE
-    )
+    board_member = models.ForeignKey(BoardMember, on_delete=models.CASCADE)
 
     attendance_status = models.CharField(
-        choices=ATTENDANCE_STATUS,
-        max_length=15,
-        blank=True,
-        null=True
+        choices=ATTENDANCE_STATUS, max_length=15, blank=True, null=True
     )
 
     def __str__(self):
-        return f'{self.board_member} {self.meeting}'
+        return f"{self.board_member}"
 
     class Meta:
-        app_label = 'board'
-        unique_together = ('meeting', 'board_member',)
-
+        app_label = "board"
+        unique_together = (
+            "meeting",
+            "board_member",
+        )
