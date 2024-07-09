ae18f55ac5f2f93f89dfe0a94c89c25882290d38
moffatmore <mofenyimoffat@gmail.com>
2024-07-09 11:14:20 +0200

    Added applicant_type in ApplicationDocumentSerializer

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -1,13 +1,20 @@
 from django.contrib.auth.models import User
 
 from rest_framework import serializers
+
 # from rest_framework.fields import CharField
 # https://docs.viewflow.io/workflow/quick_start.html
 
 
-from app.models import (Application, ApplicationStatus, ApplicationDocument, ApplicationVersion, ApplicationUser,
-                        ApplicationVerification, ApplicationRenewalHistory
-                        )
+from app.models import (
+    Application,
+    ApplicationStatus,
+    ApplicationDocument,
+    ApplicationVersion,
+    ApplicationUser,
+    ApplicationVerification,
+    ApplicationRenewalHistory,
+)
 from board.models import BoardDecision
 from workresidentpermit.models import SecurityClearance
 from app_decision.api.serializers import ApplicationDecisionTypeSerializer
@@ -18,20 +25,19 @@ class ApplicationStatusSerializer(serializers.ModelSerializer):
     class Meta:
         model = ApplicationStatus
         fields = (
-            'id',
-            'code',
-            'name',
-            'processes',
-            'valid_from',
-            'valid_to',
+            "id",
+            "code",
+            "name",
+            "processes",
+            "valid_from",
+            "valid_to",
         )
 
 
 class ApplicationUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = ApplicationUser
-        fields = (
-            'id', 'full_name', 'user_identifier', 'work_location_code', 'dob')
+        fields = ("id", "full_name", "user_identifier", "work_location_code", "dob")
 
 
 class ApplicationDocumentSerializer(serializers.ModelSerializer):
@@ -40,12 +46,13 @@ class ApplicationDocumentSerializer(serializers.ModelSerializer):
     class Meta:
         model = ApplicationDocument
         fields = (
-            'id',
-            'document_number',
-            'document_date',
-            'signed_date',
-            'applicant',
-            'submission_customer',
+            "id",
+            "document_number",
+            "document_date",
+            "signed_date",
+            "applicant",
+            "applicant_type",
+            "submission_customer",
         )
 
 
@@ -56,15 +63,15 @@ class ApplicationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Application
         fields = (
-            'id',
-            'last_application_version_id',
-            'application_type',
-            'application_status',
-            'application_document',
-            'batched',
-            'board',
-            'security_clearance',
-            'verification'
+            "id",
+            "last_application_version_id",
+            "application_type",
+            "application_status",
+            "application_document",
+            "batched",
+            "board",
+            "security_clearance",
+            "verification",
         )
 
 
@@ -73,16 +80,15 @@ class ApplicationVersionSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ApplicationVersion
-        fields = (
-            'application',
-            'version_number'
-        )
+        fields = ("application", "version_number")
 
 
 class NewApplicationSerializer(serializers.Serializer):
     process_name = serializers.CharField(max_length=200, required=True)
     full_name = serializers.CharField(max_length=200, required=True)
-    applicant_identifier = serializers.CharField(allow_blank=False, max_length=200, required=True)
+    applicant_identifier = serializers.CharField(
+        allow_blank=False, max_length=200, required=True
+    )
     work_place = serializers.CharField(allow_blank=False, max_length=200, required=True)
     status = serializers.CharField(max_length=30)
     dob = serializers.CharField(max_length=30)
@@ -90,29 +96,30 @@ class NewApplicationSerializer(serializers.Serializer):
 
 class RenewalApplicationSerializer(serializers.Serializer):
     process_name = serializers.CharField(max_length=200, required=True)
-    applicant_identifier = serializers.CharField(allow_blank=False, max_length=200, required=True)
+    applicant_identifier = serializers.CharField(
+        allow_blank=False, max_length=200, required=True
+    )
     document_number = serializers.CharField(max_length=200, required=True)
     work_place = serializers.CharField(allow_blank=False, max_length=200, required=True)
 
 
 class ApplicationVerificationSerializer(serializers.ModelSerializer):
     decision = ApplicationDecisionTypeSerializer()
-    outcome_reason = serializers.CharField(max_length=200, required=False, allow_blank=True)
+    outcome_reason = serializers.CharField(
+        max_length=200, required=False, allow_blank=True
+    )
 
     class Meta:
         model = ApplicationVerification
-        fields = (
-            'document_number',
-            'decision',
-            'outcome_reason',
-            'comment'
-        )
+        fields = ("document_number", "decision", "outcome_reason", "comment")
 
 
 class ApplicationVerificationRequestSerializer(serializers.Serializer):
     decision = serializers.CharField(max_length=200, required=True)
     comment = serializers.CharField(max_length=500, required=False, allow_blank=True)
-    outcome_reason = serializers.CharField(max_length=300, required=False, allow_blank=True)
+    outcome_reason = serializers.CharField(
+        max_length=300, required=False, allow_blank=True
+    )
 
 
 class ApplicationRenewalDTOSerializer(serializers.Serializer):
@@ -125,9 +132,4 @@ class ApplicationRenewalHistorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ApplicationRenewalHistory
-        fields = (
-            "application_type",
-            "comment",
-            "process_name",
-            "historical_record"
-        )
+        fields = ("application_type", "comment", "process_name", "historical_record")
