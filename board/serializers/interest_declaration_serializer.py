f5833f57ddf3d46e39061cb9aaa1d9837a2fa1e4
moffatmore <mofenyimoffat@gmail.com>
2024-07-10 17:24:41 +0200

    fix(Interest Declaration): :fire: Fixed interest declaration serializer, meeting attendee serializer

--- board/serializers/interest_declaration_serializer.py
+++ board/serializers/interest_declaration_serializer.py
@@ -3,29 +3,32 @@ from rest_framework import serializers
 from rest_framework.exceptions import PermissionDenied
 
 from app.api.common.web import APIMessage
+from board.serializers.meeting_attendee_serializer import MeetingAttendeeSerializer
+
 from ..choices import PRESENT
 from ..models import BoardMember, InterestDeclaration, MeetingAttendee
 
 
 class InterestDeclarationSerializer(serializers.ModelSerializer):
     document_number = serializers.CharField(max_length=150, required=True)
-    
+    meeting_attendee = MeetingAttendeeSerializer()
+
     class Meta:
         model = InterestDeclaration
         fields = (
-            'id',
-            'meeting_attendee',
-            'document_number',
-            'client_relationship',
-            'interest_description',
-            'decision',
-            'attendee_signature',
-            'date_signed',
-            'meeting'
+            "id",
+            "meeting_attendee",
+            "document_number",
+            "client_relationship",
+            "interest_description",
+            "decision",
+            "attendee_signature",
+            "date_signed",
+            "meeting",
         )
-        
+
     def to_internal_value(self, data):
-        request = self.context.get('request')
+        request = self.context.get("request")
         auth_user = request.user
         mutable_data = data.copy()
         board_member = BoardMember.objects.filter(user=auth_user).first()
@@ -33,33 +36,38 @@ class InterestDeclarationSerializer(serializers.ModelSerializer):
             api_message = APIMessage(
                 code=400,
                 message="Bad request",
-                details="User is not a member of any board"
+                details="User is not a member of any board",
             )
             raise PermissionDenied(api_message.to_dict())
         try:
-            meeting_attendee = MeetingAttendee.objects.get(Q(board_member=board_member) & Q(
-                attendance_status=PRESENT) & Q(meeting=mutable_data.get('meeting')))
+            meeting_attendee = MeetingAttendee.objects.get(
+                Q(board_member=board_member)
+                & Q(attendance_status=PRESENT)
+                & Q(meeting=mutable_data.get("meeting"))
+            )
         except MeetingAttendee.DoesNotExist:
             api_message = APIMessage(
                 code=400,
                 message="Bad request",
-                details="Board member is not an attendee of the meeting or absent"
+                details="Board member is not an attendee of the meeting or absent",
             )
             raise PermissionDenied(api_message.to_dict())
         else:
             try:
-                InterestDeclaration.objects.get(Q(meeting_attendee=meeting_attendee) & Q(document_number=data.get('document_number')))
+                InterestDeclaration.objects.get(
+                    Q(meeting_attendee=meeting_attendee)
+                    & Q(document_number=data.get("document_number"))
+                )
             except InterestDeclaration.DoesNotExist:
-                mutable_data['meeting_attendee'] = meeting_attendee.id
+                mutable_data["meeting_attendee"] = meeting_attendee.id
             else:
                 api_message = APIMessage(
                     code=400,
                     message="Bad request",
-                    details="Interest declaration already exists for the document number"
+                    details="Interest declaration already exists for the document number",
                 )
                 raise PermissionDenied(api_message.to_dict())
         return super().to_internal_value(mutable_data)
-    
+
     def create(self, validated_data):
         return super().create(validated_data)
-        
