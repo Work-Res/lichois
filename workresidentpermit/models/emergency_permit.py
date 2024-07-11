ef81c025c6f3e7bf16e23c24ad1c1b74e7d140a6
moffatmore <mofenyimoffat@gmail.com>
2024-07-11 10:52:25 +0200

    fix(Application): :fire: Added permit period in application document

--- workresidentpermit/models/emergency_permit.py
+++ workresidentpermit/models/emergency_permit.py
@@ -1,6 +1,5 @@
 from django.db import models
 from app.models import ApplicationBaseModel
-from ..choices import EMERGENCY_PERIOD
 
 
 class EmergencyPermit(ApplicationBaseModel):
@@ -9,10 +8,6 @@ class EmergencyPermit(ApplicationBaseModel):
     services_provided = models.CharField(max_length=255)
     chief_authorization = models.CharField(max_length=255)
     capacity = models.CharField(max_length=255)
-    emergency_period = models.CharField(
-        max_length=255,
-        choices=EMERGENCY_PERIOD,
-    )
 
     class Meta:
         verbose_name = "Emergency Permit"
