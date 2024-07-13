b090c867f22d39272cc9832cf04fa676eb51af83
moffatmore <mofenyimoffat@gmail.com>
2024-07-13 12:31:58 +0200

    Make agenda field optional

--- board/models/agenda.py
+++ board/models/agenda.py
@@ -6,9 +6,14 @@ from .application_batch import ApplicationBatch
 
 
 class Agenda(BaseUuidModel):
-	meeting = models.ForeignKey(BoardMeeting, on_delete=models.CASCADE)
-	description = models.TextField()
-	duration = models.PositiveIntegerField()
-	status = models.CharField(max_length=50, choices=AGENDA_STATUS, default='pending')
-	notes = models.TextField(blank=True, null=True)
-	application_batch = models.ForeignKey(ApplicationBatch, on_delete=models.SET_NULL, blank=True, null=True)
+    meeting = models.ForeignKey(BoardMeeting, on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)
+    duration = models.PositiveIntegerField(blank=True, null=True)
+    status = models.CharField(max_length=50, choices=AGENDA_STATUS, default="pending")
+    notes = models.TextField(blank=True, null=True)
+    application_batch = models.ForeignKey(
+        ApplicationBatch,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
