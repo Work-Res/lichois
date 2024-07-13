a33b096cbb1727ebff6bd381daaaba9542fb1fdd
moffatmore <mofenyimoffat@gmail.com>
2024-07-13 16:24:36 +0200

    refactor(BlueCard): :fire: fix management command to populate command

--- blue_card/apps.py
+++ blue_card/apps.py
@@ -1,6 +1,6 @@
-from django.apps import AppConfig
+from django.apps import AppConfig as BaseAppConfig
 
 
-class BlueCardConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blue_card'
+class AppConfig(BaseAppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "blue_card"
