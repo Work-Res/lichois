4a72499ca1be9b704c43031048929694d4f9b799
moffatmore <mofenyimoffat@gmail.com>
2024-07-16 15:01:34 +0200

    fix blue card management command

--- app/classes/application_service.py
+++ app/classes/application_service.py
@@ -36,6 +36,7 @@ class ApplicationService:
             return None
 
         application_status = self._get_application_status()
+        print(application_status)
         if not application_status:
             return None
 
@@ -43,6 +44,7 @@ class ApplicationService:
             return None
 
         application = self._create_application_record(application_status)
+        print(application)
         application_version = self._create_application_version(application)
 
         serializer = ApplicationVersionSerializer(application_version)
