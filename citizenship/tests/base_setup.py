9a49154b51ce7c733cda65f9bd38cae72000c5fc
Tshepiso Setsiba <tsetsiba@burs.org.bw>
2024-07-18 22:52:52 +0200

    enhance checklist

--- citizenship/tests/base_setup.py
+++ citizenship/tests/base_setup.py
@@ -7,13 +7,15 @@ from faker import Faker
 
 from app.models import ApplicationStatus
 from app.classes import ApplicationService
-from app_checklist.classes import CreateChecklistService
 
 from app.api import NewApplicationDTO
 
 from app_personal_details.models import Person, Passport
 from app_address.models import ApplicationAddress, Country
 from app_contact.models import ApplicationContact
+
+from django.apps import apps
+from app_checklist.apps import AppChecklistConfig
 
 from app_checklist.models import ClassifierItem
 from app_attachments.models import ApplicationAttachment, AttachmentDocumentType
@@ -26,6 +28,13 @@ from citizenship.utils import CitizenshipProcessEnum
 
 
 class BaseSetup(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        app_config = apps.get_app_config('app_checklist')
+        if isinstance(app_config, AppChecklistConfig):
+            app_config.ready()
 
     def create_new_application(self):
         self.new_application_dto = NewApplicationDTO(
@@ -41,33 +50,6 @@ class BaseSetup(TestCase):
         self.application_service = ApplicationService(
             new_application_dto=self.new_application_dto)
         return self.application_service.create_application()
-
-    def create_checklist_records(self):
-        file_name = "attachment_documents.json"
-        output_file = os.path.join(os.getcwd(), "app_checklist", "data", "checklist", file_name)
-
-        checklist_service = CreateChecklistService(
-            parent_classifier_name="classifiers",
-            child_name="classifier_items",
-            foreign_name="checklist_classifier",
-            parent_app_label_model_name="app_checklist.checklistclassifier",
-            foreign_app_label_model_name="app_checklist.checklistclassifieritem")
-
-        checklist_service.create(file_location=output_file)
-
-        folder_path = os.path.join(os.getcwd(), "app_checklist", "data", "workflow")
-        # Get a list of all JSON files in the folder
-        file_list = glob.glob(os.path.join(folder_path, '*.json'))
-
-        for file in file_list:
-            if os.path.isfile(file):
-                workflow_service = CreateChecklistService(
-                    parent_classifier_name="classifiers",
-                    child_name="classifier_items",
-                    foreign_name="classifier",
-                    parent_app_label_model_name="app_checklist.classifier",
-                    foreign_app_label_model_name="app_checklist.classifieritem")
-                workflow_service.create(file_location=file)
 
     def create_application_statuses(self):
         for status in statuses:
@@ -110,7 +92,6 @@ class BaseSetup(TestCase):
 
     def setUp(self) -> None:
 
-        self.create_checklist_records()
         self.create_application_statuses()
         application_version = self.create_new_application()
 
