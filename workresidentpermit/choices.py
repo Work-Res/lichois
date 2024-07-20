ddb8856f6504b9d551b98beb3f1cf34da1f9482f
moffatmore <mofenyimoffat@gmail.com>
2024-07-20 13:54:56 +0200

    fix merge conflicts

--- workresidentpermit/choices.py
+++ workresidentpermit/choices.py
@@ -1,118 +1,103 @@
+NOT_APPLICABLE = "N/A"
+OPEN = "open"
+CLOSED = "closed"
 
-NOT_APPLICABLE = 'N/A'
-OPEN = 'open'
-CLOSED = 'closed'
 
-
-COMM_METHODS = (
-    ('email', 'E-mail'),
-    ('sms', 'SMS'),
-    ('postal', 'Post')
-)
+COMM_METHODS = (("email", "E-mail"), ("sms", "SMS"), ("postal", "Post"))
 
 DATE_ESTIMATED_NA = (
-    (NOT_APPLICABLE, 'Not applicable'),
-    ('not_estimated', 'No.'),
-    ('D', 'Yes, estimated the Day'),
-    ('MD', 'Yes, estimated Month and Day'),
-    ('YMD', 'Yes, estimated Year, Month and Day'),
+    (NOT_APPLICABLE, "Not applicable"),
+    ("not_estimated", "No."),
+    ("D", "Yes, estimated the Day"),
+    ("MD", "Yes, estimated Month and Day"),
+    ("YMD", "Yes, estimated Year, Month and Day"),
 )
 
 DATE_ESTIMATED = (
-    ('-', 'No'),
-    ('D', 'Yes, estimated the Day'),
-    ('MD', 'Yes, estimated Month and Day'),
-    ('YMD', 'Yes, estimated Year, Month and Day'),
+    ("-", "No"),
+    ("D", "Yes, estimated the Day"),
+    ("MD", "Yes, estimated Month and Day"),
+    ("YMD", "Yes, estimated Year, Month and Day"),
 )
 
-GENDER = (
-	('male', 'Male'),
-	('female', 'Female'),
-	('other', 'OTHER')
-)
+GENDER = (("male", "Male"), ("female", "Female"), ("other", "OTHER"))
 
 IDENTITY_TYPE = (
-    ('OMANG', 'Omang'),
-    ('DRIVERS', 'Driver\'s License'),
-    ('PASSPORT', 'Passport'),
-    ('OMANG_RCPT', 'Omang Receipt'),
-    ('OTHER', 'Other'),
+    ("OMANG", "Omang"),
+    ("DRIVERS", "Driver's License"),
+    ("PASSPORT", "Passport"),
+    ("OMANG_RCPT", "Omang Receipt"),
+    ("OTHER", "Other"),
 )
 
 MARITAL_STATUS = (
-	('single', 'Single'),
-	('married', 'Married'),
-	('widowed', 'Widowed'),
-	('separated', 'Separated'),
-	('divorced', 'Divorced')
+    ("single", "Single"),
+    ("married", "Married"),
+    ("widowed", "Widowed"),
+    ("separated", "Separated"),
+    ("divorced", "Divorced"),
 )
 
-PERMIT_TYPE = (
-	('work', 'Work'),
-	('residence', 'Residence')
-)
+PERMIT_TYPE = (("work", "Work"), ("residence", "Residence"))
 
 POSTAL_PREFIX = (
-	('VB#', 'VB#'),
-	('private_bag', 'Private Bag'),
-	('p_o_box', 'P. O. Box')
+    ("VB#", "VB#"),
+    ("private_bag", "Private Bag"),
+    ("p_o_box", "P. O. Box"),
 )
 
 
-PREFERRED_METHOD_COMM = (
-	('sms', 'SMS'),
-	('post', 'POST'),
-	('email', 'EMAIL')
-)
+PREFERRED_METHOD_COMM = (("sms", "SMS"), ("post", "POST"), ("email", "EMAIL"))
 
 REASONS_PERMIT = (
-	('dependent', 'Dependent'),
-	('volunteer', 'Volunteer'),
-	('student', 'Student'),
-	('immigrant', 'Immigrant'),
-	('missionary', 'Missionary'),
+    ("dependent", "Dependent"),
+    ("volunteer", "Volunteer"),
+    ("student", "Student"),
+    ("immigrant", "Immigrant"),
+    ("missionary", "Missionary"),
 )
 
 REPORT_STATUS = (
-    (OPEN, 'Open. Some information is still pending.'),
-    (CLOSED, 'Closed. This report is complete'),
+    (OPEN, "Open. Some information is still pending."),
+    (CLOSED, "Closed. This report is complete"),
 )
 
-YES_NO = (
-	('yes', 'Yes'),
-	('no', 'No')
-)
+YES_NO = (("yes", "Yes"), ("no", "No"))
 
 CERTIFICATE_STATUS = (
-	('issued', 'Issued'),
-	('not_issued', 'Not Issued'),
-	('lost', 'Lost'),
-	('stolen', 'Stolen'),
-	('damaged', 'Damaged')
+    ("issued", "Issued"),
+    ("not_issued", "Not Issued"),
+    ("lost", "Lost"),
+    ("stolen", "Stolen"),
+    ("damaged", "Damaged"),
 )
 
 APPEAL_TYPE = (
-	('appeal', 'Appeal'),
-	('review', 'Review'),
-	('renewal', 'Renewal'),
-	('reconsideration', 'Reconsideration')
+    ("appeal", "Appeal"),
+    ("review", "Review"),
+    ("renewal", "Renewal"),
+    ("reconsideration", "Reconsideration"),
 )
 
 APPEAL_STATUS = (
-	('pending', 'Pending'),
-	('rejected', 'Rejected'),
-	('accepted', 'Accepted')
+    ("pending", "Pending"),
+    ("rejected", "Rejected"),
+    ("accepted", "Accepted"),
 )
 
-SUBMITTER_TYPE = (
-	('applicant', 'Applicant'),
-	('officer', 'Officer')
-)
+SUBMITTER_TYPE = (("applicant", "Applicant"), ("officer", "Officer"))
 
 PERSON_TYPE = (
-	('applicant', 'Applicant'),
-	('mother', 'Mother'),
-	('father', 'Father'),
-	('child', 'Child'),
-	('guardian', 'Guardian')
+    ("applicant", "Applicant"),
+    ("mother", "Mother"),
+    ("father", "Father"),
+    ("child", "Child"),
+    ("guardian", "Guardian"),
+)
+SUBMITTER_TYPE = (("applicant", "Applicant"), ("officer", "Officer"))
+
+EMERGENCY_PERIOD = (
+    ("1 - 14 days", "1 - 14 days"),
+    ("15 - 90 days", "15 - 90 days"),
+    ("6 months", "6 months"),
 )
--- workresidentpermit/models/emergency_permit.py
+++ workresidentpermit/models/emergency_permit.py
@@ -1,13 +1,18 @@
 from django.db import models
 from app.models import ApplicationBaseModel
+from ..choices import EMERGENCY_PERIOD
 
 
 class EmergencyPermit(ApplicationBaseModel):
-	nature_emergency = models.CharField(max_length=255)
-	job_requirements = models.CharField(max_length=255)
-	services_provided = models.CharField(max_length=255)
-	chief_authorization = models.CharField(max_length=255)
-	capacity = models.CharField(max_length=255)
-	
-	class Meta:
-		verbose_name = 'Emergency Permit'
+    nature_emergency = models.CharField(max_length=255)
+    job_requirements = models.CharField(max_length=255)
+    services_provided = models.CharField(max_length=255)
+    chief_authorization = models.CharField(max_length=255)
+    capacity = models.CharField(max_length=255)
+    emergency_period = models.CharField(
+        max_length=255,
+        choices=EMERGENCY_PERIOD,
+    )
+
+    class Meta:
+        verbose_name = "Emergency Permit"
