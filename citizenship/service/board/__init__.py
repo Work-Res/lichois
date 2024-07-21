b25fdd19103902f1d4e505d3d361cb8ebf87bc18
Tshepiso Setsiba <tsetsiba@burs.org.bw>
2024-07-21 12:22:26 +0200

    fix import error

--- citizenship/service/board/__init__.py
+++ citizenship/service/board/__init__.py
@@ -3,3 +3,5 @@ from .notification_service import NotificationService
 from .batch_service import BatchService
 from .board_service import BoardService
 from .interview_question_service import InterviewQuestionService
+from .interview_decision_service import InterviewDecisionService
+
--- citizenship/views/board/interview_decision_viewset.py
+++ citizenship/views/board/interview_decision_viewset.py
@@ -5,6 +5,7 @@ from django.core.exceptions import ValidationError
 
 from citizenship.api.serializers.board import InterviewDecisionSerializer
 from citizenship.models.board import InterviewDecision
+from citizenship.service.board import InterviewDecisionService
 
 
 class DecisionViewSet(viewsets.ViewSet):
@@ -27,8 +28,8 @@ class DecisionViewSet(viewsets.ViewSet):
 
     def retrieve(self, request, pk=None):
         try:
-            decision = DecisionService.get_decision(pk)
-            serializer = DecisionSerializer(decision)
+            decision = InterviewDecisionService.get_decision(pk)
+            serializer = InterviewDecisionSerializer(decision)
             return Response(serializer.data)
         except ValidationError as e:
             return Response({'detail': str(e)}, status=status.HTTP_404_NOT_FOUND)
@@ -37,15 +38,15 @@ class DecisionViewSet(viewsets.ViewSet):
         passed = request.data.get('passed')
         reason = request.data.get('reason')
         try:
-            decision = DecisionService.update_decision(pk, passed, reason)
-            serializer = DecisionSerializer(decision)
+            decision = InterviewDecisionService.update_decision(pk, passed, reason)
+            serializer = InterviewDecisionSerializer(decision)
             return Response(serializer.data)
         except ValidationError as e:
             return Response({'detail': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, pk=None):
         try:
-            DecisionService.delete_decision(pk)
+            InterviewDecisionService.delete_decision(pk)
             return Response(status=status.HTTP_204_NO_CONTENT)
         except ValidationError as e:
             return Response({'detail': str(e)}, status=status.HTTP_400_BAD_REQUEST)
