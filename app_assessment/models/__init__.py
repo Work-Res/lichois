a4a87e81e9663d3cc1dc9afa28388ceca67d3d24
moffatmore <mofenyimoffat@gmail.com>
2024-07-22 16:47:58 +0200

    fix merge conflict

--- app_assessment/models/__init__.py
+++ app_assessment/models/__init__.py
@@ -6,10 +6,6 @@ from .assessment_case_summary import AssessmentCaseSummary
 from .new_assessment_investor import NewAssessmentInvestor
 from .renewal_assessment_investor import RenewalAssessmentInvestor
 from .assessment_emergency import AssessmentEmergency
-<<<<<<< HEAD
 from .assessment_result import AssessmentResult
 from .parent_manager import ParentModelManager
 from .dependant_assessment import DependantAssessment
-=======
-from .appeal_assessment import AppealAssessment
->>>>>>> 147fdc3 (feat(AppealAssessment): :sparkles: Added appeal assessment endpoints)
