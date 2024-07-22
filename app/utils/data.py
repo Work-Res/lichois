cea3d0446b5655dd6b3726c0f7962a82c2b06b0a
moffatmore <mofenyimoffat@gmail.com>
2024-07-22 17:00:50 +0200

    fix merge conflict

--- app/utils/data.py
+++ app/utils/data.py
@@ -1,87 +1,83 @@
 from datetime import date
 
-statuses = [{
-    'code': 'New',
-    'name': 'NEW',
-    'processes': 'WORK_RESIDENT_PERMIT,SPECIAL_PERMIT,CITIZENSHIP_RENUNCIATION,CITIZENSHIP_INTENTION_FOREIGN_SPOUSE',
-    'valid_from': date(2023, 1, 1),
-    'valid_to': None
-},
+statuses = [
     {
-        'code': 'draft',
-        'name': 'Draft',
-        'processes': 'WORK_RESIDENT_PERMIT,SPECIAL_PERMIT,CITIZENSHIP_RENUNCIATION,CITIZENSHIP_INTENTION_FOREIGN_SPOUSE',
-        'valid_from': date(2023, 1, 1),
-        'valid_to': None
+        "code": "New",
+        "name": "NEW",
+        "processes": "WORK_RESIDENT_PERMIT,SPECIAL_PERMIT,CITIZENSHIP_RENUNCIATION,CITIZENSHIP_INTENTION_FOREIGN_SPOUSE",
+        "valid_from": date(2023, 1, 1),
+        "valid_to": None,
     },
     {
-        'code': 'VERIFICATION',
-        'name': 'Verification',
-        'processes': 'WORK_RESIDENT_PERMIT,SPECIAL_PERMIT,CITIZENSHIP_RENUNCIATION,CITIZENSHIP_INTENTION_FOREIGN_SPOUSE',
-        'valid_from': date(2023, 1, 1),
-        'valid_to': None
+        "code": "draft",
+        "name": "Draft",
+        "processes": "WORK_RESIDENT_PERMIT,SPECIAL_PERMIT,CITIZENSHIP_RENUNCIATION,CITIZENSHIP_INTENTION_FOREIGN_SPOUSE",
+        "valid_from": date(2023, 1, 1),
+        "valid_to": None,
     },
     {
-        'code': 'vetting',
-        'name': 'Vetting',
-        'processes': 'WORK_RESIDENT_PERMIT,SPECIAL_PERMIT,CITIZENSHIP_RENUNCIATION,CITIZENSHIP_INTENTION_FOREIGN_SPOUSE',
-        'valid_from': date(2023, 1, 1),
-        'valid_to': None
+        "code": "VERIFICATION",
+        "name": "Verification",
+        "processes": "WORK_RESIDENT_PERMIT,SPECIAL_PERMIT,CITIZENSHIP_RENUNCIATION,CITIZENSHIP_INTENTION_FOREIGN_SPOUSE",
+        "valid_from": date(2023, 1, 1),
+        "valid_to": None,
     },
     {
-        'code': 'committee_evaluation',
-        'name': 'COMMITTEE EVALUATION',
-        'processes': 'WORK_RESIDENT_PERMIT,SPECIAL_PERMIT,CITIZENSHIP_RENUNCIATION,CITIZENSHIP_INTENTION_FOREIGN_SPOUSE',
-        'valid_from': date(2023, 1, 1),
-        'valid_to': None
+        "code": "vetting",
+        "name": "Vetting",
+        "processes": "WORK_RESIDENT_PERMIT,SPECIAL_PERMIT,CITIZENSHIP_RENUNCIATION,CITIZENSHIP_INTENTION_FOREIGN_SPOUSE",
+        "valid_from": date(2023, 1, 1),
+        "valid_to": None,
     },
     {
-        'code': 'rejected',
-        'name': 'REJECTED',
-        'processes': 'WORK_RESIDENT_PERMIT,SPECIAL_PERMIT,CITIZENSHIP_RENUNCIATION,CITIZENSHIP_INTENTION_FOREIGN_SPOUSE',
-        'valid_from': date(2023, 1, 1),
-        'valid_to': None
+        "code": "committee_evaluation",
+        "name": "COMMITTEE EVALUATION",
+        "processes": "WORK_RESIDENT_PERMIT,SPECIAL_PERMIT,CITIZENSHIP_RENUNCIATION,CITIZENSHIP_INTENTION_FOREIGN_SPOUSE",
+        "valid_from": date(2023, 1, 1),
+        "valid_to": None,
     },
     {
-        'code': 'accepted',
-        'name': 'ACCEPTED',
-        'processes': 'WORK_RESIDENT_PERMIT,SPECIAL_PERMIT,CITIZENSHIP_RENUNCIATION,CITIZENSHIP_INTENTION_FOREIGN_SPOUSE',
-        'valid_from': date(2023, 1, 1),
-        'valid_to': None
+        "code": "rejected",
+        "name": "REJECTED",
+        "processes": "WORK_RESIDENT_PERMIT,SPECIAL_PERMIT,CITIZENSHIP_RENUNCIATION,CITIZENSHIP_INTENTION_FOREIGN_SPOUSE",
+        "valid_from": date(2023, 1, 1),
+        "valid_to": None,
     },
     {
-        'code': 'final_decision',
-        'name': 'FINAL_DECISION',
-        'processes': 'WORK_RESIDENT_PERMIT,SPECIAL_PERMIT,CITIZENSHIP_RENUNCIATION,CITIZENSHIP_INTENTION_FOREIGN_SPOUSE',
-        'valid_from': date(2023, 1, 1),
-        'valid_to': None
+        "code": "accepted",
+        "name": "ACCEPTED",
+        "processes": "WORK_RESIDENT_PERMIT,SPECIAL_PERMIT,CITIZENSHIP_RENUNCIATION,CITIZENSHIP_INTENTION_FOREIGN_SPOUSE",
+        "valid_from": date(2023, 1, 1),
+        "valid_to": None,
     },
     {
-        'code': 'recommendation',
-        'name': 'RECOMMENDATION',
-        'processes': 'WORK_RESIDENT_PERMIT,SPECIAL_PERMIT,CITIZENSHIP_RENUNCIATION,CITIZENSHIP_INTENTION_FOREIGN_SPOUSE',
-        'valid_from': date(2023, 1, 1),
-        'valid_to': None
+        "code": "final_decision",
+        "name": "FINAL_DECISION",
+        "processes": "WORK_RESIDENT_PERMIT,SPECIAL_PERMIT,CITIZENSHIP_RENUNCIATION,CITIZENSHIP_INTENTION_FOREIGN_SPOUSE",
+        "valid_from": date(2023, 1, 1),
+        "valid_to": None,
     },
     {
-        'code': 'deferred',
-        'name': 'DEFERRED',
-        'processes': 'WORK_RESIDENT_PERMIT,SPECIAL_PERMIT,CITIZENSHIP_RENUNCIATION,CITIZENSHIP_INTENTION_FOREIGN_SPOUSE',
-        'valid_from': date(2023, 1, 1),
-        'valid_to': None
+        "code": "recommendation",
+        "name": "RECOMMENDATION",
+        "processes": "WORK_RESIDENT_PERMIT,SPECIAL_PERMIT,CITIZENSHIP_RENUNCIATION,CITIZENSHIP_INTENTION_FOREIGN_SPOUSE",
+        "valid_from": date(2023, 1, 1),
+        "valid_to": None,
     },
     {
-        'code': 'pending',
-        'name': 'Pending',
-        'processes': 'WORK_RESIDENT_PERMIT,SPECIAL_PERMIT,CITIZENSHIP_RENUNCIATION,CITIZENSHIP_INTENTION_FOREIGN_SPOUSE',
-        'valid_from': date(2023, 1, 1),
-        'valid_to': None
-    }
+        "code": "deferred",
+        "name": "DEFERRED",
+        "processes": "WORK_RESIDENT_PERMIT,SPECIAL_PERMIT,CITIZENSHIP_RENUNCIATION,CITIZENSHIP_INTENTION_FOREIGN_SPOUSE",
+        "valid_from": date(2023, 1, 1),
+        "valid_to": None,
+    },
+    {
+        "code": "pending",
+        "name": "Pending",
+        "processes": "WORK_RESIDENT_PERMIT,SPECIAL_PERMIT,CITIZENSHIP_RENUNCIATION,CITIZENSHIP_INTENTION_FOREIGN_SPOUSE",
+        "valid_from": date(2023, 1, 1),
+        "valid_to": None,
+    },
 ]
 
-offices = [
-    {
-        "name": "Gaborone",
-        "code": "01"
-    }
-]
+offices = [{"name": "Gaborone", "code": "01"}]
