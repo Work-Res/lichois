56c7689569deaab1d50b2ba7a621b7a03d1e183b
moffatmore <mofenyimoffat@gmail.com>
2024-07-22 17:04:22 +0200

    fix merge conflict

--- app_assessment/models/__init__.py
+++ app_assessment/models/__init__.py
@@ -3,9 +3,17 @@ from .assessement_case_decision import AssessmentCaseDecision
 from .assessment import Assessment
 from .assessment_case_note import AssessmentCaseNote
 from .assessment_case_summary import AssessmentCaseSummary
+<<<<<<< HEAD
 from .new_assessment_investor import NewAssessmentInvestor
 from .renewal_assessment_investor import RenewalAssessmentInvestor
 from .assessment_emergency import AssessmentEmergency
 from .assessment_result import AssessmentResult
 from .parent_manager import ParentModelManager
 from .dependant_assessment import DependantAssessment
+=======
+from .assessment_emergency import AssessmentEmergency
+from .assessment_result import AssessmentResult
+from .new_assessment_investor import NewAssessmentInvestor
+from .parent_manager import ParentModelManager
+from .renewal_assessment_investor import RenewalAssessmentInvestor
+>>>>>>> b5834d2 (fix merge conflicts)
