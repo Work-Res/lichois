4a62633819ea403a9facd0f645685e41cfdbfafd
moffatmore <mofenyimoffat@gmail.com>
2024-07-23 21:39:25 +0200

    fix merge conflicts

--- blue_card/models/blue_card.py
+++ blue_card/models/blue_card.py
@@ -4,9 +4,5 @@ from app.models.application_base_model import ApplicationBaseModel
 class BlueCard(ApplicationBaseModel):
 
     class Meta:
-<<<<<<< HEAD
         verbose_name = "Blue Card"
         verbose_name_plural = "Blue Cards"
-=======
-        app_label = "visa"
->>>>>>> aa5e5a9 (feat(BlueCard): :sparkles: Added new service (blue card))
