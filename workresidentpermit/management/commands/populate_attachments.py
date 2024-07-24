c0c4e9a44c7b8a3377c1b6f7db131d4dfb3aacc0
moffatmore <mofenyimoffat@gmail.com>
2024-07-24 16:21:55 +0200

    fix attachments

--- workresidentpermit/management/commands/populate_attachments.py
+++ workresidentpermit/management/commands/populate_attachments.py
@@ -25,7 +25,7 @@ class Command(BaseCommand):
                 document_number=app.document_number
             )
             if not application.exists():
-                for _ in range(randint(1, 2) + 1):
+                for _ in range(randint(2)):
                     document_type = AttachmentDocumentType.objects.create(
                         code=faker.random_int(min=1000, max=9999),
                         name=faker.random_element(
