4fbf8f9edc7505994f383f235e728c8186810e51
moffatmore <mofenyimoffat@gmail.com>
2024-07-25 17:04:14 +0200

    deactivate activate next activity after verification

--- app/service/base_decision_service.py
+++ app/service/base_decision_service.py
@@ -109,9 +109,12 @@ class BaseDecisionService(UpdateApplicationMixin):
             field_key=self.application_field_key,
             field_value=application_decision_type.code.upper(),
         )
+        self.logger.info(
+            f"Application {self.request.document_number} decision created successfully."
+        )
         # self._create_comment()
         # self._deactivate_current_task()
-        self._activate_next_task()
+        # self._activate_next_task()
 
         return self.response.result()
 
