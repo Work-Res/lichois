4c593520b1d7c5f5c7cdfd513000c0dfd482cc78
moffatmore <mofenyimoffat@gmail.com>
2024-07-26 15:44:08 +0200

    Added visa summary endpoint

--- visa/api/viewsets/visa_application_viewset.py
+++ visa/api/viewsets/visa_application_viewset.py
@@ -1,9 +1,29 @@
+from requests import Response
 from rest_framework import viewsets
+from rest_framework.decorators import action
 
-from ..serializers import VisaApplicationSerializer
+from app.classes.application_summary import ApplicationSummary
+
 from ...models import VisaApplication
+from ..serializers import VisaApplicationSerializer
+
+
+def get_app_labels():
+    return [
+        "visa.VisaApplication",
+    ]
 
 
 class VisaApplicationViewSet(viewsets.ModelViewSet):
     queryset = VisaApplication.objects.all()
     serializer_class = VisaApplicationSerializer
+
+    @action(
+        detail=False,
+        methods=["get"],
+        url_path="summary/(?P<document_number>[A-Za-z0-9-]+)",
+        url_name="visa-summary",
+    )
+    def summary(self, request, document_number):
+        app_summary = ApplicationSummary(document_number, get_app_labels())
+        return Response(data=app_summary.data())
