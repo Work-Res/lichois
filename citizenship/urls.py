d63e4148bd85d210bfd8864b7dd3860ad3fe9383
Tshepiso Setsiba <tsetsiba@burs.org.bw>
2024-08-02 13:07:22 +0200

    meeting sessions

--- citizenship/urls.py
+++ citizenship/urls.py
@@ -3,13 +3,14 @@ from django.urls.conf import path
 from rest_framework.routers import DefaultRouter
 
 from .admin_site import citizenship_admin
-from .views.board import BoardModelViewSet, MeetingViewSet, ScoreSheetViewSet
+from .views.board import BoardModelViewSet, MeetingViewSet, ScoreSheetViewSet, MeetingSessionViewSet
 
 app_name = 'citizenship'
 
 router = DefaultRouter()
 router.register(r'citizenship-boards', BoardModelViewSet)
 router.register(r'citizenship-meetings', MeetingViewSet, basename='meeting')
+router.register(r'citizenship-meeting-sessions', MeetingSessionViewSet)
 router.register(r'citizenship-scoresheets', ScoreSheetViewSet)
 
 
