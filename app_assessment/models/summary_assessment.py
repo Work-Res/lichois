4ef92d25def7b8bea91ca18890e15e686fc19cb7
moffatmore <mofenyimoffat@gmail.com>
2024-08-11 12:47:22 +0200

    fix summary assessment

--- app_assessment/models/summary_assessment.py
+++ app_assessment/models/summary_assessment.py
@@ -4,4 +4,4 @@ from app.models import ApplicationBaseModel
 
 
 class SummaryAssessment(ApplicationBaseModel):
-    summary = models.TextField(blank=True, null=True)
+    summary = models.TextField()
