f304c960eadabd0f8a5de963a6c01db14b9485f5
Tshepiso Setsiba <tsetsiba@burs.org.bw>
2024-08-11 16:41:55 +0200

    meeting id

--- citizenship/views/board/filter/batch_application_filter.py
+++ citizenship/views/board/filter/batch_application_filter.py
@@ -7,6 +7,7 @@ class BatchApplicationFilter(django_filters.FilterSet):
     batch = django_filters.UUIDFilter(field_name='batch__id', lookup_expr='exact')
     application = django_filters.UUIDFilter(field_name='application__id', lookup_expr='exact')
     meeting_session = django_filters.UUIDFilter(field_name='meeting_session__id', lookup_expr='exact')
+    meeting = django_filters.UUIDFilter(field_name='meeting_session__meeting__id', lookup_expr='exact')
 
     class Meta:
         model = BatchApplication
