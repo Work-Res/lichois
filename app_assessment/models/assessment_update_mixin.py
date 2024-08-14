2debccc9e85488768342fa48359ef074997a9c3f
moffatmore <mofenyimoffat@gmail.com>
2024-08-14 17:55:32 +0200

    fix assessment update mixin

--- app_assessment/models/assessment_update_mixin.py
+++ app_assessment/models/assessment_update_mixin.py
@@ -1,13 +1,14 @@
 from app.classes.mixins.update_application_mixin import UpdateApplicationMixin
+from app.utils.system_enums import ApplicationDecisionEnum
 
 
 class AssessmentUpdateMixin(UpdateApplicationMixin):
 
     def save(self, *args, **kwargs):
         # Update the related application's assessment field to "done"
-        if hasattr(self, "document_number") and self.document_number:
+        if self.document_number:
             self.update_application_field(
                 document_number=self.document_number,
                 field_key="assessment",
-                field_value="done",
+                field_value=ApplicationDecisionEnum.DONE.value,
             )
