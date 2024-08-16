6188b7b871944cb94d714edf5eed2b773c301a9a
TTshimo <ttshimo16@gmail.com>
2024-08-16 12:06:31 +0200

    non-citizen-profile

--- non_citizen_profile/api/serializers/__init__.py
+++ non_citizen_profile/api/serializers/__init__.py
@@ -3,4 +3,6 @@ from api.serializers.profile import BiometricsSerializer
 from api.serializers.profile import ContactDetailsSerializer
 from api.serializers.profile import PassportSerializer
 from api.serializers.profile import PersonalDetailsSerializer
+from api.serializers.profile import EducationSerializer
+from api.serializers.profile import NextOfKinSerializer
 from api.serializers.profile import CombinedSerializer
---
+++ non_citizen_profile/models/education.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from app_personal_details.choices import EDUCATION_LEVELS
+
+from app.models import ApplicationBaseModel
+
+
+class Education(ApplicationBaseModel):
+
+    level = models.CharField(max_length=50, choices=EDUCATION_LEVELS)
+    field_of_study = models.CharField(max_length=100)
+    institution = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.level} in {self.field_of_study} from {self.institution}"
