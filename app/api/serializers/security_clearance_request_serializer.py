d8e1eb669ff8691965614f6b5cf1d13cf7203393
moffatmore <mofenyimoffat@gmail.com>
2024-08-19 14:47:07 +0200

    allow summary to be blank for security clearance

--- app/api/serializers/security_clearance_request_serializer.py
+++ app/api/serializers/security_clearance_request_serializer.py
@@ -3,4 +3,4 @@ from rest_framework import serializers
 
 class SecurityClearanceRequestDTOSerializer(serializers.Serializer):
     status = serializers.CharField(max_length=200, required=True)
-    summary = serializers.CharField(max_length=500, required=False)
+    summary = serializers.CharField(max_length=500, required=False, allow_blank=True)
