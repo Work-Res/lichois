522909b23504ae0da69edc41834dd8896336f540
moffatmore <mofenyimoffat@gmail.com>
2024-08-19 15:15:43 +0200

    fix minister decision view, allow blank summary

--- app/api/serializers/minister_decision_serializer.py
+++ app/api/serializers/minister_decision_serializer.py
@@ -6,7 +6,7 @@ from ...models import MinisterDecision
 class MinisterDecisionRequestDTOSerializer(serializers.Serializer):
     document_number = serializers.CharField(max_length=200, required=True)
     status = serializers.CharField(max_length=200, required=True)
-    summary = serializers.CharField(max_length=500, required=False)
+    summary = serializers.CharField(max_length=500, required=False, allow_blank=True)
 
 
 class MinisterDecisionSerializer(serializers.Serializer):
