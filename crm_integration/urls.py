4046006266da4bb33f847c60b3dc9a16817e24bd
Coulson Kgathi <ckgathi@gmail.com>
2024-08-20 09:56:01 +0200

    remove forward slash

--- crm_integration/urls.py
+++ crm_integration/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .api.views import CRMFormSubmitView
 
 urlpatterns = [
-    path("crm/submit/", CRMFormSubmitView.as_view(), name="crm_form_submit"),
+    path("crm/submit", CRMFormSubmitView.as_view(), name="crm_form_submit"),
 ]
