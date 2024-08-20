2752dba8a71691550fe52522f3dd7bb31daf170d
moffatmore <mofenyimoffat@gmail.com>
2024-08-20 13:41:03 +0200

    fix allow summary blank in commissioner decision

--- app/api/serializers/recommendation_request_serializer.py
+++ app/api/serializers/recommendation_request_serializer.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class RecommendationRequestDTOSerializer(serializers.Serializer):
     document_number = serializers.CharField(max_length=200, required=True)
     status = serializers.CharField(max_length=200, required=True)
-    summary = serializers.CharField(max_length=500, required=False)
+    summary = serializers.CharField(max_length=500, required=False, allow_blank=True)
