9c431aa3ac943003b8c99fee0fda399c21ad157f
moffatmore <mofenyimoffat@gmail.com>
2024-08-23 16:00:11 +0200

    fixed permanent residence workflow

--- permanent_residence/management/commands/populate_permanent_residence.py
+++ permanent_residence/management/commands/populate_permanent_residence.py
@@ -10,13 +10,15 @@ from permanent_residence.models.permanent_residence import PermanentResidence
 class Command(CustomBaseCommand):
     help = "Populate data for Populate data for Blue card service"
     process_name = ApplicationProcesses.PERMANENT_RESIDENCE.value
-    application_type = (
-        PermanentResidenceApplicationTypeEnum.PERMANENT_RESIDENCE_ONLY.value,
-        PermanentResidenceApplicationTypeEnum.PERMANENT_RESIDENCE_10_YEARS.value,
+    faker = Faker()
+    application_type = faker.random_element(
+        elements=(
+            PermanentResidenceApplicationTypeEnum.PERMANENT_RESIDENCE_ONLY.value,
+            PermanentResidenceApplicationTypeEnum.PERMANENT_RESIDENCE_10_YEARS.value,
+        )
     )
 
     def handle(self, *args, **options):
-        faker = Faker()
         self.stdout.write(self.style.SUCCESS(f"Process name {self.process_name}"))
 
         for _ in range(150):
@@ -27,23 +29,23 @@ class Command(CustomBaseCommand):
                 PermanentResidence.objects.get_or_create(
                     application_version=version,
                     document_number=app.application_document.document_number,
-                    investor_contribution=faker.text(),
-                    employee_contribution=faker.text(),
-                    any_other_resident=faker.name(),
-                    violated_terms_before=faker.text(),
-                    preferred_method_comm=faker.text(),
-                    language=faker.text(),
-                    spouse_applying_residence=faker.text(),
-                    prohibited_in_botswana=faker.text(),
-                    reasons_prohibited=faker.text(),
-                    imprisonment_any_country=faker.text(),
-                    reasons_imprisonment=faker.text(),
-                    permit_reasons=faker.text(),
-                    period_when_required=faker.date_this_century(),
-                    take_up_occupation=faker.text(),
-                    application_reason=faker.text(),
-                    application_reason_other=faker.text(),
-                    application_status=faker.text(),
+                    investor_contribution=self.faker.text(),
+                    employee_contribution=self.faker.text(),
+                    any_other_resident=self.faker.name(),
+                    violated_terms_before=self.faker.text(),
+                    preferred_method_comm=self.faker.text(),
+                    language=self.faker.text(),
+                    spouse_applying_residence=self.faker.text(),
+                    prohibited_in_botswana=self.faker.text(),
+                    reasons_prohibited=self.faker.text(),
+                    imprisonment_any_country=self.faker.text(),
+                    reasons_imprisonment=self.faker.text(),
+                    permit_reasons=self.faker.text(),
+                    period_when_required=self.faker.date_this_century(),
+                    take_up_occupation=self.faker.text(),
+                    application_reason=self.faker.text(),
+                    application_reason_other=self.faker.text(),
+                    application_status=self.faker.text(),
                 )
 
                 self.stdout.write(
