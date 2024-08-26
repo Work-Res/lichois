82f0ddbf7ba4cd01ab70bca8b3a2f9645662bb75
Tshepiso Setsiba <tsetsiba@burs.org.bw>
2024-08-26 17:50:51 +0200

    api for gazette batchs applicaitons

--- gazette/tests/test_token.py
+++ gazette/tests/test_token.py
@@ -6,10 +6,10 @@ from ..service.email_receiver.oauth.token import get_oauth2_token
 class TestToken(TestCase):
 
     def test_token(self):
-        token = get_oauth2_token(
+        response = get_oauth2_token(
             "173d20ad-b66c-49f4-abf4-e18fafe84417",
             "5dm8Q~MJoDfJEAJIcIbWIbCA1akmtDVFyeSiGauM",
             "cad42c6a-07cb-4b36-88b3-aa1995b4e3ca",
         )
-        print(token)
-        self.assertIsNotNone(token)
+        self.assertIsNotNone(response)
+        self.assertIn("access_token", container=response)
