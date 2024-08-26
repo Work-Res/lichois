0e604cf6aa25326986a568c0fe65756bbea1865d
Tshepiso Setsiba <tsetsiba@burs.org.bw>
2024-08-26 20:19:04 +0200

    fix filter

--- gazette/views/filter/batch_application_filter.py
+++ gazette/views/filter/batch_application_filter.py
@@ -3,8 +3,8 @@ from gazette.models import BatchApplication
 
 
 class BatchApplicationFilter(django_filters.FilterSet):
-    batch = django_filters.CharFilter(field_name='batch__name', lookup_expr='icontains')
-    application = django_filters.CharFilter(field_name='application__applicant_name', lookup_expr='icontains')
+    batch = django_filters.CharFilter(field_name='batch__title', lookup_expr='icontains')
+    application = django_filters.CharFilter(field_name='application__application_document__document_number', lookup_expr='icontains')
     included_in_final_list = django_filters.BooleanFilter()
     reviewed_by_legal = django_filters.BooleanFilter()
     reviewed_by_ag = django_filters.BooleanFilter()
