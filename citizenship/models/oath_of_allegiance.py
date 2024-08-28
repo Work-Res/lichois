87f99e630aa949263cb46d59f656329a56e652c7
Tshepiso Setsiba <tsetsiba@burs.org.bw>
2024-08-28 13:41:17 +0200

    fix relationship

--- citizenship/models/oath_of_allegiance.py
+++ citizenship/models/oath_of_allegiance.py
@@ -3,9 +3,10 @@ from base_module.model_mixins import BaseUuidModel
 from base_module.model_mixins import DeclarationModelMixin
 from base_module.model_mixins import CommissionerOathModelMixin
 
+from app.models import ApplicationBaseModel
 
-class OathOfAllegiance(DeclarationModelMixin, CommissionerOathModelMixin,
-                       BaseUuidModel):
+
+class OathOfAllegiance(ApplicationBaseModel, DeclarationModelMixin, CommissionerOathModelMixin):
 
     class Meta:
         app_label = 'citizenship'
