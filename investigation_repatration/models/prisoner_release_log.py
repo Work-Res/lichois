c3f0be3d6126aaadd1813d2690d19d516582cf9c
moffatmore <mofenyimoffat@gmail.com>
2024-09-11 09:45:30 +0200

    refactor: 🔥 Added other fields on prisoner release log

--- investigation_repatration/models/prisoner_release_log.py
+++ investigation_repatration/models/prisoner_release_log.py
@@ -4,3 +4,8 @@ from .prisoner import Prisoner
 
 class PrisonerReleaseLog(models.Model):
     prisoners = models.ManyToManyField(Prisoner, null=True, blank=True)
+    date_of_issue = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    batch_release_date = models.DateTimeField(null=True, blank=True)
+    facility_name = models.CharField(max_length=100, null=True, blank=True)
+    status = models.CharField(max_length=50, null=True, blank=True)
+    batch_size = models.IntegerField(null=True, blank=True)
