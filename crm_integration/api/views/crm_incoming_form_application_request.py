9e395f448c8b51bdbb701b77663d6f54fed5a0c2
Coulson Kgathi <ckgathi@gmail.com>
2024-10-01 08:45:42 +0200

    update/delete API views

--- crm_integration/api/views/crm_incoming_form_application_request.py
+++ crm_integration/api/views/crm_incoming_form_application_request.py
@@ -1,30 +1,19 @@
-import logging
-
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
-from crm_integration.service import RabbitMQService
 
 
 class CrmIncomingFormApplicationRequest(APIView):
 
     permission_classes = [AllowAny]
 
-    def post(self, request, *args, **kwargs):
-
-        try:
-            logging.INFO(f"Recievied a requested from CRM. {request.data}")
-            rabbitmq_service = RabbitMQService(
-                queue_name="citizenship_queue", exchange_key="citizenship_exchange"
-            )
-            rabbitmq_service.publish_message(message=request.data)
-            return Response(
-                {"message": "Form submitted successfully"}, status=status.HTTP_200_OK
-            )
-
-        except Exception as e:
-            return Response(
-                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
-            )
+    def post(self, request):
+        # Retrieve the data from the request
+        data = request.data
+        
+        # Process the data (You can add your custom logic here)
+        print(data)
+        
+        # Return the processed data as a response
+        return Response({'processed_data': "processed_data coli"}, status=status.HTTP_200_OK)
