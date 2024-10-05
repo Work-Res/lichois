cb067d852967ee8e00005707c6f5c6d715b6b09b
TTshimo <ttshimo16@gmail.com>
2024-10-05 17:07:23 +0200

    blue card

--- app_contact/models/application_contact.py
+++ app_contact/models/application_contact.py
@@ -3,19 +3,25 @@ from django.db import models
 from app.models import ApplicationBaseModel
 
 from .choices import CONTACT_TYPES
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class ApplicationContact(ApplicationBaseModel):
 
     creator = models.CharField(max_length=255, blank=True, null=True)
     modifier = models.CharField(max_length=255, blank=True, null=True)
-    status = models.CharField(max_length=255, blank=True, null=True)
     country_code = models.CharField(max_length=10, blank=True, null=True)
     contact_type = models.CharField(choices=CONTACT_TYPES, max_length=100, blank=False, null=False)
     sub_type = models.CharField(max_length=255, blank=True, null=True)
     contact_value = models.CharField(max_length=255, blank=False, null=False)
-    description = models.TextField(blank=True, null=True)
     preferred_method_comm = models.BooleanField(default=False)
+    email = models.EmailField()
+    cell = models.IntegerField(
+    validators=[
+        MinValueValidator(10000000),  
+        MaxValueValidator(99999999)   
+    ]
+)
 
     def __str__(self):
         return f" {self.contact_type} - {self.contact_value}"
