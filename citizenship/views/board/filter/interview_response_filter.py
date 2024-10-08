7cf6868941379e618090f5dfcfd98925c197ca22
Tshepiso Setsiba <tsetsiba@burs.org.bw>
2024-10-08 21:35:36 +0200

    fix filter

--- citizenship/views/board/filter/interview_response_filter.py
+++ citizenship/views/board/filter/interview_response_filter.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 class InterviewResponseFilter(django_filters.FilterSet):
     interview = django_filters.UUIDFilter(field_name='interview__id', lookup_expr='exact')
-    member = django_filters.UUIDFilter(field_name='member__user', lookup_expr='exact')
+    member = django_filters.UUIDFilter(method='filter_by_member')  # Custom method applied here
     score = django_filters.NumberFilter(field_name='score', lookup_expr='exact')
     score_min = django_filters.NumberFilter(field_name='score', lookup_expr='gte')
     score_max = django_filters.NumberFilter(field_name='score', lookup_expr='lte')
@@ -20,29 +20,23 @@ class InterviewResponseFilter(django_filters.FilterSet):
     # Filter by Meeting ID
     meeting = django_filters.UUIDFilter(field_name='interview__meeting__id', lookup_expr='exact')
 
-    # Filter by Meeting Title (for example)
+    # Filter by Meeting Title
     meeting_title = django_filters.CharFilter(field_name='interview__meeting__title', lookup_expr='icontains')
 
     def __init__(self, *args, **kwargs):
-        # Extract request from kwargs
+        # Extract the request object from kwargs
         self.request = kwargs.pop('request', None)
         self.user = self.request.user
-        logger.info(f"Empty empty:  {self.request.user }")
-
         super().__init__(*args, **kwargs)
-        logger.info(f"Empty empty after:  { self.user }")
 
-        if self.user:
-            logger.info(f"Applying filter for user: {self.user}")
-            # Modify the queryset to filter by the request.user
-            self.filters['member'].extra.update(
-                {'method': self.filter_by_request_user}
-            )
+        if self.request:
+            logger.info(f"Request user: {self.request.user}")
 
     def filter_by_member(self, queryset, name, value):
         """
-        Custom filter method for member field.
-        If member is not provided, use request.user.
+        Custom filter method for the 'member' field.
+        If the 'member' value is provided in the query params, it filters by that.
+        Otherwise, it filters by the current request user.
         """
         if value:
             return queryset.filter(member__user=value)
