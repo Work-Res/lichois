e3bb7976c8673e57cc919230baac17be64a0ca8a
samKenpachi011 <samuelkabelo1@gmail.com>
2024-10-09 17:02:24 +0200

    fix: tests for president 10b decision

--- app/views/tests/test_pres_10b_decision_api.py
+++ app/views/tests/test_pres_10b_decision_api.py
@@ -35,6 +35,12 @@ def create_pres_decision(document_number, **params):
     decision = PresRecommendationDecision.objects.create(document_number=document_number, **defaults)
     return decision
 
+
+def detail_url(decision_id):
+    """Create and return a  detail URL."""
+    return reverse('pres-recommendation-decision-create', args=[decision_id])
+
+
 class PublicPresDecision10BTests(BaseSetup):
     """Tests fot unauthenticated users"""
 
@@ -75,7 +81,6 @@ class PrivatePresDecision10BTests(BaseSetup):
             new_application_dto=self.new_application_dto)
         return self.application_service.create_application()
 
-
     def test_retrieve_decision(self):
         """Test retrieve Pres Recommendation Decision"""
 
@@ -92,3 +97,46 @@ class PrivatePresDecision10BTests(BaseSetup):
         # self.assertEqual(res.status_code, status.HTTP_200_OK)
         # self.assertEqual(res.data, serializer.data)
         self.assertEqual(decisions.count(), 1)
+
+    def test_retrieve_decision_without_document_number(self):
+        """Test retrieve Pres Recommendation Decision without document number"""
+        res = self.client.get(PRES_DECISION_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(res.data, {"detail": "Document number is required"})
+
+
+
+    # def test_create_pres_decision(self):
+    #     """Test creating a Pres Recommendation Decision"""
+
+    #     app = Application.objects.get(
+    #         application_document__document_number=self.document_number)
+
+    #     payload = {
+    #         'document_number': app.application_document.document_number,
+    #         'status': ApplicationDecisionEnum.ACCEPTED.value,
+    #         'approved_by': 'TEST_USER',
+    #         'role': 'PRESIDENT'
+    #     }
+    #     res = self.client.post(PRES_DECISION_URL, payload, format='json')
+    #     # self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+    #     decision = PresRecommendationDecision.objects.get(document_number=app.application_document.document_number)
+    #     # serializer = PresRecommendationDecisionSerializer(decisions, many=True)
+
+    # #     # self.assertEqual(res.status_code, status.HTTP_200_OK)
+    # #     # self.assertEqual(res.data, serializer.data)
+    #     self.assertEqual(decision.count(), 1)
+    #     self.assertEqual(decision.status, ApplicationDecisionEnum.ACCEPTED.value)
+
+    # def test_create_pres_decision_invalid_data(self):
+    #     """Test creating a Pres Recommendation Decision with invalid data"""
+    #     payload = {
+    #         'document_number': '',  # Invalid document number
+    #         'status': ApplicationDecisionEnum.ACCEPTED.value,
+    #         'approved_by': 'TEST_USER',
+    #         'role': 'PRESIDENT'
+    #     }
+    #     res = self.client.post(PRES_DECISION_URL, payload)
+    #     self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
