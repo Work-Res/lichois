6d2b4cc4327facd3b064fb2e729b84f220383d8b
Tshepiso Setsiba <tsetsiba@burs.org.bw>
2024-10-10 20:33:54 +0200

    fix pre_recommandation api

--- app/api/serializers/pres_recommendation_decision_serializer.py
+++ app/api/serializers/pres_recommendation_decision_serializer.py
@@ -4,6 +4,11 @@ from app.models import PresRecommendationDecision
 
 
 class PresRecommendationDecisionSerializer(serializers.ModelSerializer):
+
+    document_number = serializers.CharField(max_length=200, required=True)
+    status = serializers.CharField(max_length=200, required=True)
+    summary = serializers.CharField(max_length=500, required=False, allow_blank=True)
+
     class Meta:
         model = PresRecommendationDecision
         fields = [
