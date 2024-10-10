3fce2e8c101c9b04538d6c1481e917011a7ef257
moffatmore <mofenyimoffat@gmail.com>
2024-10-10 21:20:22 +0200

    fix cell and email field in app_contact

--- app_contact/models/application_contact.py
+++ app_contact/models/application_contact.py
@@ -11,12 +11,14 @@ class ApplicationContact(ApplicationBaseModel):
     creator = models.CharField(max_length=255, blank=True, null=True)
     modifier = models.CharField(max_length=255, blank=True, null=True)
     country_code = models.CharField(max_length=10, blank=True, null=True)
-    contact_type = models.CharField(choices=CONTACT_TYPES, max_length=100, blank=False, null=False)
+    contact_type = models.CharField(
+        choices=CONTACT_TYPES, max_length=100, blank=False, null=False
+    )
     sub_type = models.CharField(max_length=255, blank=True, null=True)
     contact_value = models.CharField(max_length=255, blank=False, null=False)
     preferred_method_comm = models.BooleanField(default=False)
-    email = models.EmailField()
-    cell = models.CharField(max_length=20)
+    email = models.EmailField(null=True, blank=True, max_length=255)
+    cell = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
         return f" {self.contact_type} - {self.contact_value}"
