7db5384ccd260b086a044a9c9092d18b06578d89
Coulson Kgathi <ckgathi@gmail.com>
2024-10-12 12:33:54 +0200

    api update

--- crm_integration/api/views/work_residence_permit_api_view.py
+++ crm_integration/api/views/work_residence_permit_api_view.py
@@ -3,6 +3,9 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from ...classes import CrmRequestHandler
+
+
 
 class WorkResidenceAPIView(APIView):
 
@@ -10,10 +13,38 @@ class WorkResidenceAPIView(APIView):
 
     def post(self, request):
         # Retrieve the data from the request
-        data = request.data
+        data = self.build_master_dict(request)
         
         # Process the data (You can add your custom logic here)
-        print(data)
+        self.process_data(build_master_dict=data)
         
         # Return the processed data as a response
         return Response({'processed_data': "processed_data coli"}, status=status.HTTP_200_OK)
+
+    def process_data(self, build_master_dict=None):
+        """Extract form data and create or update instances.
+        """
+        crm_request = CrmRequestHandler(
+            crm_request=build_master_dict)
+
+        return crm_request.group_and_trim_keys()
+    
+    def build_master_dict(self, request):
+        """Build and return master form data dictionary.
+        """
+        payload = request.data["payload"]
+        form_data = payload["form"]
+        
+        # Input data
+        data = list(form_data.values())
+        merged_data_dict = {}
+        
+        for d in data:
+            merged_data_dict.update(d)
+        return merged_data_dict
+
+
+    def create_application(self):
+        """
+        """
+        pass
--- crm_integration/classes/crm_request_handler.py
+++ crm_integration/classes/crm_request_handler.py
@@ -1,15 +1,15 @@
+from django.apps import apps
 
 
 class CrmRequestHandler:
 
-    def __init__(self, crm_request, crm_request_substring_configuration):
+    def __init__(self, crm_request):
         self.crm_request = crm_request
-        self.crm_request_substring_configuration = crm_request_substring_configuration
 
     def group_and_trim_keys(self):
         result = []
 
-        for substring in self.crm_request_substring_configuration:
+        for substring in self.crm_req_substring_config:
             # Filter the main_dict to get keys containing the substring
             filtered_dict = {k: v for k, v in self.crm_request.items() if substring in k}
 
@@ -20,3 +20,19 @@ class CrmRequestHandler:
             result.append({substring: trimmed_dict})
 
         return result
+
+    @property
+    def crm_req_substring_config(self):
+        """Returns apps and models list.
+        """
+
+        # Loop through all installed apps
+        substrings = []
+        for app_config in apps.get_app_configs():
+            app_label = app_config.label
+            # Loop through all models in the app
+            for model in app_config.get_models():
+                model_name = model.__name__
+                # Join app label and model name with an underscore
+                substrings.append(f"{app_label}_{model_name}")
+        return substrings
