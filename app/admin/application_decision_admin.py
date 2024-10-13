97aa44c94956645c98128414c681df8f7f43c5f2
moffatmore <mofenyimoffat@gmail.com>
2024-10-13 14:02:38 +0200

    added app decision and decision type admin

--- app/admin/application_decision_admin.py
+++ app/admin/application_decision_admin.py
@@ -7,12 +7,14 @@ class ApplicationDecisionTypeAdmin(admin.ModelAdmin):
     list_display = ('code', 'name', 'process_types', 'valid_from', 'valid_to')
     search_fields = ('code', 'name', 'process_types')
     list_filter = ('valid_from', 'valid_to')
+    model = ApplicationDecisionType
 
 class ApplicationDecisionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'final_decision_type', 'proposed_decision_type', 'created_at', 'updated_at')
+    list_display = ('id', 'final_decision_type', 'proposed_decision_type')
     search_fields = ('final_decision_type__name', 'proposed_decision_type__name')
-    list_filter = ('final_decision_type', 'proposed_decision_type', 'created_at', 'updated_at')
+    list_filter = ('final_decision_type', 'proposed_decision_type')
     raw_id_fields = ('final_decision_type', 'proposed_decision_type')
+    model = ApplicationDecision
     
 app_admin.register(ApplicationDecision, ApplicationDecisionAdmin)
 app_admin.register(ApplicationDecisionType, ApplicationDecisionTypeAdmin)
