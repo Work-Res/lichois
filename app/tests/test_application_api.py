276bc460a460d0b66c04a46f2f46b601934eebaa
samKenpachi011 <samuelkabelo1@gmail.com>
2024-10-17 22:33:25 +0200

    FIX: application sorting using the list endpoint

--- app/tests/test_application_api.py
+++ app/tests/test_application_api.py
@@ -1,15 +1,19 @@
-import pytest
 import json
 
+from django.urls import reverse
 from rest_framework.test import APIClient
-from rest_framework.test import APITestCase
 from rest_framework import status
+from citizenship.tests.base_setup import BaseSetup
+from django.contrib.auth import get_user_model
+from app.api import NewApplicationDTO
+from app.classes import ApplicationService
+from citizenship.utils.citizenship_process_enum import CitizenshipProcessEnum
+from app.utils import ApplicationStatusEnum
+from app.models import Application
 
-# from app.api import NewApplicationDTO
-
-pytestmark = pytest.mark.django_db
 
 
+APPLICATIONS_URL = reverse('application-list')
 class TestApplicationApi:
 
     """
@@ -33,3 +37,92 @@ class TestApplicationApi:
         response = self.client.get(application_status_endpoint)
         assert response.status_code == status.HTTP_200_OK
         assert len(json.loads(response.content)) == 1
+
+
+class PrivateApplicationOrderBy(BaseSetup):
+
+    """Tests for authenticated users"""
+    def setUp(self) -> None:
+        super().setUp()
+        self.client = APIClient()
+
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            password='testpassword')
+
+        self.client.force_authenticate(self.user)
+
+
+        self.new_application_dto = NewApplicationDTO(
+            process_name=CitizenshipProcessEnum.PRESIDENT_POWER_10B.value,
+            applicant_identifier='317918515',
+            status=ApplicationStatusEnum.VERIFICATION.value,
+            dob="06101990",
+            work_place="01",
+            application_type=CitizenshipProcessEnum.PRESIDENT_POWER_10B.value,
+            full_name="Test test",
+            applicant_type="student"
+        )
+
+        self.new_application_dto2 = NewApplicationDTO(
+            process_name=CitizenshipProcessEnum.PRESIDENT_POWER_10B.value,
+            applicant_identifier='428019626',
+            status=ApplicationStatusEnum.VERIFICATION.value,
+            dob="06101990",
+            work_place="01",
+            application_type=CitizenshipProcessEnum.PRESIDENT_POWER_10B.value,
+            full_name="Test test 2",
+            applicant_type="immigrant"
+        )
+
+        self.application_service = ApplicationService(
+            new_application_dto=self.new_application_dto)
+
+        self.application_service2 = ApplicationService(
+            new_application_dto=self.new_application_dto2)
+
+        self.application_service.create_application()
+        self.application_service2.create_application()
+
+    def test_list_applications_default_ordering(self):
+
+
+        res = self.client.get(APPLICATIONS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        applications = Application.objects.all()
+
+        dates = [app.created for app in applications]
+
+        self.assertEqual(dates, sorted(dates))  #
+
+
+    def test_list_applications_descending_order(self):
+        """Test that the applications are returned in descending order by created date."""
+        url = APPLICATIONS_URL + '?sort_by=created&order=desc'
+        res = self.client.get(url)
+        results = res.data['results']
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+        created_dates = [app['application_status']['created'] for app in results]
+
+        self.assertEqual(created_dates, sorted(created_dates, reverse=True))
+
+    def test_invalid_ordering_field(self):
+        """
+        Test that an invalid sorting field returns a 400 .
+        """
+        url = APPLICATIONS_URL + '?sort_by=invalid_field'
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_invalid_order_value(self):
+        """
+        Test that an invalid order value returns a 400.
+        """
+        url = APPLICATIONS_URL + '?sort_by=created&order=invalid'
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
--- app/views/application_list_view.py
+++ app/views/application_list_view.py
@@ -4,6 +4,7 @@ from rest_framework.pagination import PageNumberPagination
 from app.models import Application
 from app.api.serializers import ApplicationSerializer
 from app.views.filters.application_filter import ApplicationModelFilter
+from rest_framework.exceptions import ValidationError
 
 
 class StandardResultsSetPagination(PageNumberPagination):
@@ -16,3 +17,26 @@ class ApplicationListView(viewsets.ModelViewSet):
     serializer_class = ApplicationSerializer
     filterset_class = ApplicationModelFilter
     pagination_class = StandardResultsSetPagination
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+
+        sort_by = self.request.query_params.get('sort_by', 'created')
+        order_by = self.request.query_params.get('order', 'asc')
+
+        allowed_sort_fields = ['created']
+
+        if sort_by and sort_by not in allowed_sort_fields:
+            raise ValidationError(f"Invalid sort field: {sort_by}")
+
+        if order_by not in ['asc', 'desc']:
+            raise ValidationError(f"Invalid order value: {order_by}")
+
+        if sort_by:
+            if order_by == 'desc':
+                sort_by = f'-{sort_by}'
+            else:
+                sort_by = sort_by
+
+        return queryset.order_by(sort_by)
