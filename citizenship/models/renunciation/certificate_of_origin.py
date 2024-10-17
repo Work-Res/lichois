6a47751ede79acb3576f4dc3472c112ec6283d43
Tshepiso Setsiba <tsetsiba@burs.org.bw>
2024-10-17 23:18:13 +0200

    add

--- citizenship/models/renunciation/certificate_of_origin.py
+++ citizenship/models/renunciation/certificate_of_origin.py
@@ -8,13 +8,17 @@ from citizenship.models import KgosiCertificate, KgosanaCertificate
 
 class CertificateOfOrigin(BaseUuidModel):
 
-    father = models.ForeignKey(Person, on_delete=models.CASCADE, null=True, blank=True, related_name="fathers")
+    father = models.ForeignKey(Person, on_delete=models.CASCADE, null=True, blank=True,
+                               related_name="certificate_of_origin_fathers")
 
-    mother = models.ForeignKey(Person, on_delete=models.CASCADE, null=True, blank=True, related_name="mothers")
+    mother = models.ForeignKey(Person, on_delete=models.CASCADE, null=True, blank=True,
+                               related_name="certificate_of_origin_mothers")
 
-    kgosi = models.ForeignKey(KgosiCertificate, on_delete=models.CASCADE, null=True, blank=True)
+    kgosi = models.ForeignKey(KgosiCertificate, on_delete=models.CASCADE, null=True, blank=True,
+                              related_name="certificate_of_origin_kgosi")
 
-    kgosana = models.ForeignKey(KgosanaCertificate, on_delete=models.CASCADE, null=True, blank=True)
+    kgosana = models.ForeignKey(KgosanaCertificate, on_delete=models.CASCADE, null=True, blank=True,
+                                related_name="certificate_of_origin_kgosana")
 
     class Meta:
         app_label = 'citizenship'
