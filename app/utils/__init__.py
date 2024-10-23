8c267f670580c50cfc1827eed210c02cb334405b
moffatmore <mofenyimoffat@gmail.com>
2024-10-23 11:50:09 +0200

    Fixed application status

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,4 +1,9 @@
-from .system_enums import ApplicationProcesses, ApplicationStatusEnum, WorkflowEnum, ApplicationDecisionEnum
-from .data import statuses
+from .system_enums import (
+    ApplicationProcesses,
+    ApplicationStatusEnum,
+    WorkflowEnum,
+    ApplicationDecisionEnum,
+)
+from .data import statuses, decision_types
 from .verification_status_enums import VerificationStatusEnum
 from .module_process_name_enum import ModuleProcessNameEnum
