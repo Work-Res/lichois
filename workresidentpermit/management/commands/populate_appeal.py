cfba84a41f1e933c9fc889c6818dce78400dc487
TTshimo <ttshimo16@gmail.com>
2024-10-25 23:56:56 +0200

    fixing managements commands for work and res services and visa

--- workresidentpermit/management/commands/populate_appeal.py
+++ workresidentpermit/management/commands/populate_appeal.py
@@ -1,27 +1,26 @@
 from random import randint
 
-from django.core.management.base import BaseCommand
 from django.db.transaction import atomic
-from faker import Faker
+from lichois.management.base_command import CustomBaseCommand
 
-from app.api import NewApplicationDTO
-from app.classes import ApplicationService
 from app.utils import ApplicationProcesses
-from app.utils.system_enums import ApplicationStatusEnum
-from app_address.models import ApplicationAddress, Country
-from app_contact.models import ApplicationContact
-from app_personal_details.models import Passport, Person
 from workresidentpermit.models import PermitAppeal
 from workresidentpermit.utils import WorkResidentPermitApplicationTypeEnum
 
 
-class Command(BaseCommand):
+class Command(CustomBaseCommand):
     help = "Populate data for Populate data for Emergency & Exemption model"
 
+    process_name = ApplicationProcesses.APPEAL_PERMIT.value
+    application_type = elements=(
+                        WorkResidentPermitApplicationTypeEnum.WORK_RESIDENT_PERMIT_APPEAL.value,
+                        WorkResidentPermitApplicationTypeEnum.WORK_PERMIT_APPEAL.value,
+                        WorkResidentPermitApplicationTypeEnum.RESIDENT_PERMIT_APPEAL.value,
+                        WorkResidentPermitApplicationTypeEnum.EXEMPTION_CERTIFICATE_APPEAL.value,
+                    )
     def handle(self, *args, **options):
-        faker = Faker()
-        process_name = ApplicationProcesses.APPEAL_PERMIT.name
-        self.stdout.write(self.style.SUCCESS(f"Process name {process_name}"))
+        self.stdout.write(self.style.SUCCESS(f"Process name {self.process_name}"))
+
         # ApplicationStatus.objects.get_or_create(
         # 	code=ApplicationStatusEnum.NEW.value,
         # 	name=ApplicationStatusEnum.VERIFICATION.name,
@@ -40,26 +39,18 @@ class Command(BaseCommand):
         # )
 
         for _ in range(150):
+
             with atomic():
-                self.application_type = faker.random_element(
-                    elements=(
-                        WorkResidentPermitApplicationTypeEnum.WORK_RESIDENT_PERMIT_APPEAL.value,
-                        WorkResidentPermitApplicationTypeEnum.WORK_PERMIT_APPEAL.value,
-                        WorkResidentPermitApplicationTypeEnum.RESIDENT_PERMIT_APPEAL.value,
-                        WorkResidentPermitApplicationTypeEnum.EXEMPTION_CERTIFICATE_APPEAL.value,
-                    )
-                )
-                self.stdout.write(self.style.SUCCESS("Populating appeal data..."))
                 app, version = self.create_basic_data()
 
                 PermitAppeal.objects.get_or_create(
                     application_version=version,
                     document_number=app.application_document.document_number,
-                    appeal_type=faker.random_element(
+                    appeal_type=self.faker.random_element(
                         elements=("appeal", "review", "renewal", "reconsideration")
                     ),
-                    reason_for_appeal=faker.text(),
-                    appeal_date=faker.date_this_century(),
+                    reason_for_appeal=self.faker.text(),
+                    appeal_date=self.faker.date_this_century(),
                 )
 
                 self.stdout.write(self.style.SUCCESS("Successfully populated data"))
