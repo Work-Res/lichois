c583ebce54f962f69a0dc0e3f1162426dee1e331
moffatmore <mofenyimoffat@gmail.com>
2024-10-26 11:13:00 +0200

    Fix board meeting signals, create meeting invitation when b/meeting is scheduled only

--- board/models/signals.py
+++ board/models/signals.py
@@ -6,15 +6,18 @@ from .board_meeting import BoardMeeting
 from .meeting_invitation import MeetingInvitation
 
 
-@receiver(post_save, weak=False, sender=BoardMeeting,
-          dispatch_uid='board_meeting_on_post_save')
+@receiver(
+    post_save,
+    weak=False,
+    sender=BoardMeeting,
+    dispatch_uid="board_meeting_on_post_save",
+)
 def board_meeting_on_post_save(sender, instance, raw, created, **kwargs):
-    """ Create board meeting invitations for all board members when a new board meeting is created.
-    """
-    if created:
+    """Create board meeting invitations for all board members when a new board meeting is created."""
+    if created and instance.status == "scheduled":
         board_id = instance.board_id
         board_members = None
-        
+
         try:
             attending_board = Board.objects.get(id=board_id)
         except Board.DoesNotExist:
@@ -29,29 +32,30 @@ def board_meeting_on_post_save(sender, instance, raw, created, **kwargs):
                 )
 
 
-@receiver(post_save, weak=False, sender=MeetingInvitation,
-          dispatch_uid='board_meeting_invitation_on_post_save')
+@receiver(
+    post_save,
+    weak=False,
+    sender=MeetingInvitation,
+    dispatch_uid="board_meeting_invitation_on_post_save",
+)
 def board_meeting_invitation_on_post_save(sender, instance, raw, created, **kwargs):
     if not created:
-        attendance_status = 'present' if instance.status == 'approved' else 'absent'
-        
+        attendance_status = "present" if instance.status == "approved" else "absent"
+
         # Check if a MeetingAttendee object with the given meeting_id and board_member_id already exists
         attendee_exists = MeetingAttendee.objects.filter(
-            meeting=instance.board_meeting,
-            board_member=instance.invited_user
+            meeting=instance.board_meeting, board_member=instance.invited_user
         ).exists()
-        
+
         if not attendee_exists:
             # If it doesn't exist, create a new one
             MeetingAttendee.objects.create(
                 meeting=instance.board_meeting,
                 board_member=instance.invited_user,
-                attendance_status=attendance_status
+                attendance_status=attendance_status,
             )
         else:
             # If it does exist, update it
             MeetingAttendee.objects.filter(
-                meeting=instance.board_meeting,
-                board_member=instance.invited_user
+                meeting=instance.board_meeting, board_member=instance.invited_user
             ).update(attendance_status=attendance_status)
-
