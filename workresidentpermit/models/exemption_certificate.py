cdf2200d0cd352570e8fd38cabb16ac08e300ceb
samKenpachi011 <samuelkabelo1@gmail.com>
2024-11-01 19:06:50 +0200

    ADD: field to exemption certificate

--- workresidentpermit/models/exemption_certificate.py
+++ workresidentpermit/models/exemption_certificate.py
@@ -7,6 +7,7 @@ class ExemptionCertificate(ApplicationBaseModel):
     business_name = models.CharField(max_length=150)
     employment_capacity = models.CharField(max_length=250)
     proposed_period = models.CharField(max_length=150)
+    relevant_experience = models.TextField(max_length=500, blank=True, null=True)
 
     class Meta:
         verbose_name = "Exemption Certificate"
