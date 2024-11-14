84674e50a3fa6d75853a03de984f5dcab8ab2029
Tshepiso Setsiba <tsetsiba@burs.org.bw>
2024-11-14 18:58:30 +0200

    add document_number as filter

--- app/views/application_renewal_history_filter.py
+++ app/views/application_renewal_history_filter.py
@@ -1,25 +1,21 @@
 import django_filters
-
-
-from rest_framework.pagination import PageNumberPagination
-
-from app.models import ApplicationRenewalHistory
-
-
-class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 10
-    max_page_size = 1000
+from app.models import ApplicationRenewalHistory, ApplicationDocument
 
 
 class ApplicationRenewalHistoryFilter(django_filters.FilterSet):
-
     application_type = django_filters.CharFilter(
-        field_name='application_type', lookup_expr='icontains')
-
-    user_identifier = django_filters.BooleanFilter(field_name='application_user__user_identifier')
-
+        field_name='application_type', lookup_expr='icontains'
+    )
+    user_identifier = django_filters.BooleanFilter(
+        field_name='application_user__user_identifier'
+    )
     process_name = django_filters.CharFilter(
-        field_name='process_name', lookup_expr='icontains')
+        field_name='process_name', lookup_expr='icontains'
+    )
+    document_number = django_filters.CharFilter(
+        field_name='application_user__applicationdocument__document_number',
+        lookup_expr='icontains'
+    )
 
     class Meta:
         model = ApplicationRenewalHistory
