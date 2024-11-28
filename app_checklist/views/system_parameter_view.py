a9f2e4d12dfb2d11b4deb0e359133ed7d315a874
TTshimo <ttshimo16@gmail.com>
2024-11-28 10:09:23 +0200

    add systerm parameter filter, update blue card checklists, update cancellation workflow

--- app_checklist/views/system_parameter_view.py
+++ app_checklist/views/system_parameter_view.py
@@ -2,8 +2,11 @@ from rest_framework import viewsets
 
 from ..api.serializers import SystemParameterSerializer
 from ..models import SystemParameter
-
+from django_filters import rest_framework as filters
+from ..filters import SystemParameterFilter
 
 class SystemParameterViewSet(viewsets.ModelViewSet):
     queryset = SystemParameter.objects.all()
     serializer_class = SystemParameterSerializer
+    filter_backends = (filters.DjangoFilterBackend,)  
+    filterset_class = SystemParameterFilter 
