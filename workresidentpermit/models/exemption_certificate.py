13fe1478e261c6a574aa1c0732ac980f0793572f
moffatmore <mofenyimoffat@gmail.com>
2024-11-30 01:44:29 +0200

    fix exemption certificate and education model

--- workresidentpermit/models/exemption_certificate.py
+++ workresidentpermit/models/exemption_certificate.py
@@ -1,5 +1,7 @@
 from django.db import models
 from app.models import ApplicationBaseModel
+from app_address.models import ApplicationAddress
+from app_personal_details.models import Education
 
 
 class ExemptionCertificate(ApplicationBaseModel):
@@ -7,6 +9,13 @@ class ExemptionCertificate(ApplicationBaseModel):
     business_name = models.CharField(max_length=150)
     employment_capacity = models.CharField(max_length=250)
     proposed_period = models.CharField(max_length=150)
+    business_addresss = models.ForeignKey(
+        ApplicationAddress, on_delete=models.CASCADE, null=True, blank=True
+    )
+    qualification = models.ForeignKey(
+        Education, on_delete=models.CASCADE, null=True, blank=True
+    )
+    experience = models.CharField(max_length=250, null=True, blank=True)
 
     class Meta:
         verbose_name = "Exemption Certificate"
