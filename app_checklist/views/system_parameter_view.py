9d427abfefe888e706bdfc6d9b2288330e28d32e
moffatmore <mofenyimoffat@gmail.com>
2024-11-30 01:58:55 +0200

    fix system parameter created more than once

--- app_checklist/views/system_parameter_view.py
+++ app_checklist/views/system_parameter_view.py
@@ -1,3 +1,4 @@
+from django.forms import ValidationError
 from rest_framework import viewsets
 
 from ..api.serializers import SystemParameterSerializer
@@ -5,8 +6,18 @@ from ..models import SystemParameter
 from django_filters import rest_framework as filters
 from ..filters import SystemParameterFilter
 
+
 class SystemParameterViewSet(viewsets.ModelViewSet):
     queryset = SystemParameter.objects.all()
     serializer_class = SystemParameterSerializer
-    filter_backends = (filters.DjangoFilterBackend,)  
-    filterset_class = SystemParameterFilter 
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = SystemParameterFilter
+
+    def create(self, request, *args, **kwargs):
+        document_number = request.data.get("document_number")
+        if SystemParameter.objects.filter(document_number=document_number).exists():
+            raise ValidationError(
+                {"detail": "SystemParameter with this document number already exists."}
+            )
+
+        return super().create(request, *args, **kwargs)
