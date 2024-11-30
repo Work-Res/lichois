efdb3a53df3f506953097eeea754a9022118d3df
Coulson Kgathi <ckgathi@gmail.com>
2024-11-30 05:16:17 +0200

    fix admin

--- app_contact/admin/application_contact_admin.py
+++ app_contact/admin/application_contact_admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from ..models import ApplicationContact
 
 class ApplicationContactAdmin(admin.ModelAdmin):
-    list_display = ['creator', 'modifier', 'country_code', 'sub_type', 'contact_value', 'email', 'cell']
+    list_display = ['creator', 'modifier', 'country_code', 'contact_value', 'email', 'cell']
     search_fields = ['creator', 'modifier', 'contact_value']
     
 admin.site.register(ApplicationContact, ApplicationContactAdmin)
