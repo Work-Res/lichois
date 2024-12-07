447180504a92f394dcdae31c7664f868b0939f3c
MoffatMore <mofenyimoffat@gmail.com>
2024-12-07 20:56:48 +0200

    fix minister decision accepted status

--- app_checklist/views/system_parameter_view.py
+++ app_checklist/views/system_parameter_view.py
@@ -5,6 +5,8 @@ from ..api.serializers import SystemParameterSerializer
 from ..models import SystemParameter
 from django_filters import rest_framework as filters
 from ..filters import SystemParameterFilter
+from rest_framework import status
+from rest_framework.response import Response
 
 
 class SystemParameterViewSet(viewsets.ModelViewSet):
@@ -16,8 +18,12 @@ class SystemParameterViewSet(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         document_number = request.data.get("document_number")
         if SystemParameter.objects.filter(document_number=document_number).exists():
-            raise ValidationError(
-                {"detail": "SystemParameter with this document number already exists."}
+            return Response(
+                {"detail": "SystemParameter with this document number already exists."},
+                status=status.HTTP_400_BAD_REQUEST,
             )
+            # raise ValidationError(
+            #     {"detail": "SystemParameter with this document number already exists."}
+            # )
 
         return super().create(request, *args, **kwargs)
