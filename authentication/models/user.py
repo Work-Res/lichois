81cb8a5e4b634ea139c01c77f401030eca132153
MoffatMore <mofenyimoffat@gmail.com>
2024-12-08 15:27:06 +0200

    progres

--- authentication/models/user.py
+++ authentication/models/user.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from identifier.non_citizen_identifier_model_mixins import (
+    NonUniqueNonCitizenIdentifierFieldMixin,
+    NonCitizenIdentifierMethodsModelMixin
+)
 
 
-class User(AbstractUser):
+class User(NonUniqueNonCitizenIdentifierFieldMixin, AbstractUser, NonCitizenIdentifierMethodsModelMixin):
     phone_number = models.IntegerField(unique=True, null=True)
+    dob = models.DateField(null=True, blank=True)
 
     def is_chairperson(self):
         return self.groups.filter(name="chairperson").exists()
@@ -13,6 +18,9 @@ class User(AbstractUser):
 
     def is_secretary(self):
         return self.groups.filter(name="secretary").exists()
+
+    def is_customer(self):
+        return self.groups.filter(name="customer").exists()
 
     def __str__(self) -> str:
         return self.username
