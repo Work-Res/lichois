b5f68aa1957a42000d68bf1e1561dc8889793c24
samKenpachi011 <samuelkabelo1@gmail.com>
2024-12-09 11:14:08 +0200

    ADD: person update

--- app_personal_details/admin/person_admin.py
+++ app_personal_details/admin/person_admin.py
@@ -4,5 +4,13 @@ from ..models import Person
 class PersonAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'middle_name', 'maiden_name', 'marital_status', 'dob', 'gender', 'occupation', 'qualification', 'person_type', 'deceased']
     search_fields = ['first_name', 'last_name']
-    
+    fieldsets = (
+        (None, {
+            "fields": (
+                'first_name', 'last_name',
+            ),
+        }),
+    )
+
+
 admin.site.register(Person, PersonAdmin)
