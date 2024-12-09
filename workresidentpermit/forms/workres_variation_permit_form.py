1355efca811722b50fa5f4e70ce7033d0eb526ad
samKenpachi011 <samuelkabelo1@gmail.com>
2024-12-09 11:05:47 +0200

    ADD: remove validators

--- workresidentpermit/forms/workres_variation_permit_form.py
+++ workresidentpermit/forms/workres_variation_permit_form.py
@@ -1,11 +1,11 @@
 from django import forms
 from ..models import VariationPermit
-from lichois_form_validators.form_validators import WorkResVariationPermitFormValidator
+# from lichois_form_validators.form_validators import WorkResVariationPermitFormValidator
 from lichois_form_validators import FormValidatorMixin
 
 class WorkResVariationPermitForm(FormValidatorMixin, forms.ModelForm):
 
-    form_validator_cls = WorkResVariationPermitFormValidator
+    # form_validator_cls = WorkResVariationPermitFormValidator
 
     class Meta:
         model = VariationPermit
