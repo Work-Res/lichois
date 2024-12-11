86621a63c1e2d70e39148406eca898dccaa2f1d7
tmongwato <tmongwato@gmail.com>
2024-12-11 17:54:35 +0200

    verbose names for next of kin and spouse details

--- app_personal_details/models/spouse.py
+++ app_personal_details/models/spouse.py
@@ -4,13 +4,13 @@ from .passport import Passport
 
 
 class Spouse(ApplicationBaseModel):
-    last_name = models.CharField(max_length=190)
-    first_name = models.CharField(max_length=190)
-    middle_name = models.CharField(max_length=190, blank=True, null=True)
-    maiden_name = models.CharField(max_length=190, blank=True, null=True)
-    country = models.CharField(max_length=190)
-    place_birth = models.CharField(max_length=190)
-    dob = models.DateField()
+    last_name = models.CharField(max_length=190,verbose_name="Last Name")
+    first_name = models.CharField(max_length=190,verbose_name="First Name")
+    middle_name = models.CharField(max_length=190, blank=True, null=True,verbose_name="Other Names")
+    maiden_name = models.CharField(max_length=190, blank=True, null=True,verbose_name="Previous/Maiden Surname")
+    country = models.CharField(max_length=190,verbose_name="Country of Birth of Spouse")
+    place_birth = models.CharField(max_length=190,verbose_name="Place of Birth of Spouse")
+    dob = models.DateField(verbose_name="Date of Birth of Spouse")
     passport = models.ForeignKey(
         Passport,
         on_delete=models.CASCADE,
