12025591119ef39085433128d1cbd360b11f1152
tmongwato <tmongwato@gmail.com>
2024-12-11 17:54:35 +0200

    verbose names for next of kin and spouse details

--- app_personal_details/models/next_of_kin.py
+++ app_personal_details/models/next_of_kin.py
@@ -5,30 +5,35 @@ from app.models.application_base_model import ApplicationBaseModel
 
 class NextOfKin(ApplicationBaseModel):
 
-    first_name = models.CharField(max_length=190)
+    first_name = models.CharField(max_length=190,verbose_name="First Name")
 
-    last_name = models.CharField(max_length=190)
+
+    last_name = models.CharField(max_length=190,verbose_name="Last Name")
 
     telephone = models.CharField(
         max_length=190,
         blank=True,
         null=True,
+        verbose_name="Telephone"
     )
 
     cell_phone = models.CharField(
         max_length=190,
         blank=True,
         null=True,
+        verbose_name="Cellphone"
     )
 
     relation = models.CharField(
         max_length=50,
         blank=True,
         null=True,
+        verbose_name="Relations"
     )
 
     physical_address = models.CharField(
         max_length=50,
         blank=True,
         null=True,
+        verbose_name="Physical Address"
     )
