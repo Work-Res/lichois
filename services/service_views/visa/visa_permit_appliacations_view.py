c4eb13273480a2311a6ac38460cf9e8f9520087b
Thato Will Joseph <programergeek@gmail.com>
2024-12-19 16:46:34 +0200

    fix: work and res sub process views

--- services/service_views/visa/visa_permit_appliacations_view.py
+++ services/service_views/visa/visa_permit_appliacations_view.py
@@ -2,6 +2,8 @@ from django.views.generic import TemplateView
 
 from services.form_models import visa
 from ..service_application_view_mixin import ServiceApplicationViewMixin
+from app.utils import ApplicationProcesses
+from workresidentpermit.utils import WorkResidentPermitApplicationTypeEnum
 
 
 class VisaPermitDashboardView(TemplateView, ServiceApplicationViewMixin):
@@ -9,28 +11,36 @@ class VisaPermitDashboardView(TemplateView, ServiceApplicationViewMixin):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-    
-        model_cls_list = visa # This could come from a config file
-    
+
+        model_cls_list = visa  # This could come from a config file
+
         context.update(
             new_application=self.new_application,
             create_new_application=self.create_new_application,
-            application_forms= self.application_forms(
+            application_forms=self.application_forms(
                 model_cls_list=model_cls_list),
-            
-            document_number=self.application_number(),
+
+            document_number=self.generate_new_application_number,
             non_citizen_identifier=self.non_citizen_identifier,
             personal_details=self.personal_details
-            )
+        )
 
         return context
 
+    @property
+    def generate_new_application_number(self):
+        """Return a new application number.
+        """
+        application_number = self.new_application_number(
+            process_name=ApplicationProcesses.WORK_RESIDENT_PERMIT.value,
+            application_type=WorkResidentPermitApplicationTypeEnum.WORK_RESIDENT_PERMIT_ONLY.value)
+        return application_number
 
     def permits(self):
         """Returns a list of all work and res permits.
         """
         return None
-    
+
     @property
     def new_application(self):
         """Return true if a new application is in progress.
@@ -45,4 +55,3 @@ class VisaPermitDashboardView(TemplateView, ServiceApplicationViewMixin):
         if new_app:
             # Generate a new application number
             return '123456'
- 
